369f51abc58e606425cc608e6ae8186a86a9e59b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-06-01 23:37:55 +0900

    add aa(co) and promise example

---
+++ node/postgresql-ex/ex03-aa-promise/pg-promise-light.js
@@ -0,0 +1,57 @@
+(function () {
+  var pg = require('pg');
+  var Promise = require('promise-light');
+
+  var pgpromise = {
+    pg: pg,
+    Client: function Client(connStr) {
+      var ctx = this;
+      var client = new pg.Client(connStr);
+
+      this.connect = function connect() {
+        return new Promise(function (resolve, reject) {
+          client.connect(function (err) {
+            if (err) return reject(err);
+            resolve();
+          }); // client.connect
+        }); // return new Promise()
+      }; // connect
+
+      this.query = function query(sql, params) {
+        return new Promise(function (resolve, reject) {
+          client.query(sql, params, function (err, result) {
+            if (err) return reject(err);
+            resolve(result);
+          }); // client.query
+        }); // return new Promise()
+      }; // query
+
+      this.end = client.end.bind(client);
+
+    }, // Client
+    connect: function connect(connStr) {
+      var ctx = this;
+      return new Promise(function (resolve, reject) {
+        ctx.pg.connect(connStr, function (err, client, done) {
+          if (err) return reject(err);
+          resolve({
+            client: client,
+            done: done,
+            query: function query(sql, params) {
+              return new Promise(function (resolve, reject) {
+                client.query(sql, params, function (err, result) {
+                  if (err) return reject(err);
+                  resolve(result);
+                });
+              }); // return new Promise()
+            }, // query
+          }); // resolve
+        }); // pg.connect
+      }); // return new Promise()
+    }, // connect
+    end: pg.end.bind(pg),
+  }; // pgpromise
+
+  module.exports = pgpromise;
+
+})();
