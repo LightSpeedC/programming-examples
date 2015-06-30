fc6d875e1d4cfda0fca23a020b5215bd4214c309
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-01 07:36:00 +0900

    revise

--- node/port-forward-ex/port-forward-ex.js
+++ node/port-forward-ex/port-forward-ex.js
@@ -1,22 +1,25 @@
+// port-forward-ex.js
+
 (function () {
+  'use strict';
+
   var net = require('net');
-  var aa = require('aa');
+  var aa = require('./aa');
   var config = require('./config').config;
-  var chan = require('./chan');
+  var MAX_DUMP_LEN = 360;
 
-  var server = net.createServer(function(svrSoc) {
+  var server = net.createServer(function(cliSoc) {
     //'connection' listener
-    console.log('server connected');
+    //console.log('server connected');
 
-    var cliSoc = net.connect(config.forwardPort, function () {});
+    var svrSoc = net.connect(config.forwardPort, function () {});
 
     aa(function * () {
       try {
-        yield [aa(soc2soc, svrSoc, cliSoc, 'svr2cliSoc'),
-               aa(soc2soc, cliSoc, svrSoc, 'cli2svrSoc')];
+        yield [aa(soc2soc(svrSoc, cliSoc, 'cli<-svr')),
+               aa(soc2soc(cliSoc, svrSoc, 'cli->svr'))];
       } catch (err) { error(err, 'main'); }
-      cliSoc.end();
-      svrSoc.end();
+      cliSoc.end(), svrSoc.end();
     });
 
   });
@@ -27,16 +30,20 @@
 
   console.log(config);
 
-  // thread: soc1 -> soc
-  function * soc2soc(soc1, soc, msg) {
-    var ch = chan().stream(soc1);
-    while(!ch.done()) {
-      try { var buff = yield ch; } catch (err) { return soc.end(), error(err, msg); }
-      if (buff === ch.empty) { //error('ch is empty', msg); 
-        break; }
-      try { soc.write(buff);     } catch (err) { return soc.end(), console.error(buff), error(err, msg); }
+  // thread: reader -> writer
+  function * soc2soc(reader, writer, msg) {
+    var chan = aa().stream(reader), buff = null, count = 0;
+    try {
+      while(buff = yield chan) {
+        if (count++ <= 1) console.error('%s: (%s) %s', msg, count, buff2str(buff));
+        writer.write(buff), buff = null;
+      }
+    } catch (err) {
+      if (buff !== null) console.error('%s: (%s) %s', msg, typeof buff, buff2str(buff));
+      //console.log('%s: %s: %s', msg, err.constructor.name, err.message);
+      error(err, msg);
     }
-    soc.end();
+    reader.end(), writer.end();
   }
 
   function error(err, msg) {
@@ -45,7 +52,31 @@
   }
 
   function err2str(err) {
-    return !!err && err.stack || (err + '');
+    return err + '';
+    //return !!err && err.stack || (err + '');
   }
+
+  var buff2strx = 41;
+  function buff2str(buff) {
+    if (!buff) return '<>';
+    var str = '<';
+    var n = Math.min(buff.length, MAX_DUMP_LEN);
+    //if (n > MAX_DUMP_LEN) n = MAX_DUMP_LEN;
+    //n = Math.min(n, MAX_DUMP_LEN);
+    for (var i = 0; i < n; ++i) {
+      var c = buff[i];
+      if (c >= 0x20 && c <= 0x7E) str += String.fromCharCode(c);
+      else {
+        buff2strx = 41 + 42 - buff2strx;
+        str += '\x1b[' + buff2strx + 'm' + ('0' + c.toString(16).toUpperCase()).slice(-2) + '\x1b[m';
+      }
+    }
+    return str + '>';
+  }
+
+  require('control-c')(
+    function () { console.log('ctrl-c'); },
+    function () { console.log('server.close();'); server.close(); },
+    function () { console.log('process.exit();'); process.exit(); });
 
 })();
