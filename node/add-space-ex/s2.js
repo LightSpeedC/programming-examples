ba37cbe313832f6068c2feee9ddcf304b489ecc9
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-08 09:17:35 +0900

    add space example

---
+++ node/add-space-ex/s2.js
@@ -0,0 +1,12 @@
+  inFileName=process.argv[2]
+  outFileName=process.argv[3]
+  if (!inFileName) return console.log('node s2 {in} {out}')
+  if (!outFileName) outFileName = inFileName + '.log';
+  fs=require('fs')
+  lines=fs.readFileSync(inFileName).toString().split('\r\n');
+  lines=lines.map(function(line){
+    if (line.length < 2) return line;
+    if (line.substr(0, 2) === '  ') return line.substr(2);
+    return line;
+  });
+  fs.writeFileSync(outFileName, lines.join('\r\n'));
