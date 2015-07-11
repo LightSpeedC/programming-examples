cd686251fc3341cc6ecb3373e9de90111d8a5b50
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-12 07:38:30 +0900

    revise tmp script

--- node/port-forward-ex/tmp.js
+++ node/port-forward-ex/tmp.js
@@ -1,6 +1,9 @@
   var fwd = require('./fwd').fwd;
+  var fwdHttp = require('./fwd-http').fwdHttp;
 
-  fwd({servicePort: 9999, forwardPort: 9998,
+  fwd({servicePort: 9999, forwardPort: 9997,
     logFile: 'log/fwd-%s.log', logLevel: 'trace'});
-  fwd({servicePort: 8888, forwardPort: 9998,
+  fwd({servicePort: 8888, forwardPort: 9997,
     logFile: 'log/fwd-%s.log', logLevel: 'trace'});
+  fwdHttp({servicePort: 9997, proxyUrl: 'http://localhost:9998',
+    logFile: 'log/proxy-%s.log', logLevel: 'trace'});
