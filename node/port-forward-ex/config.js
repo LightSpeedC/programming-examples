bac8eeee77dd8834c6d9687629f8ca3f9ed7fb00
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-15 06:52:14 +0900

    main & config

--- node/port-forward-ex/config.js
+++ node/port-forward-ex/config.js
@@ -1,6 +1,6 @@
 this.config = {
   servicePort: 9999,
-  proxyUrl: 'http://localhost:9998',
+  XXXproxyUrl: 'http://localhost:9998',
   logFile: 'log/fwd-%s.log',
   logLevel: 'info'
 }
---
+++ node/port-forward-ex/main.cmd
@@ -0,0 +1,4 @@
+pushd %~dp0
+iojs main
+pause
+popd
