c260af1e82af4426f48c975c8128e7af0a8f1295
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-15 06:52:27 +0900

    change filters

--- node/port-forward-ex/tmp.js
+++ node/port-forward-ex/tmp.js
@@ -1,5 +1,5 @@
   var logFile = 'log/fwd-%s.log';
-  var logLevel = 'trace';
+  var logLevel = 'debug';
   var log = require('log-manager').setWriter(new require('log-writer')(logFile)).getLogger();
   log.setLevel(logLevel);
 
@@ -7,12 +7,14 @@
   var fwdHttp = require('./fwd-http').fwdHttp;
 
   var filters = {
-    '127.*;192.168.*;nx-*;t-*;x-*;rsb00*;b000*;kok*-*;*.dev;localhost': 'http://localhost:9990',
-    '172.16.*;172.17.*;rssv*;*.group': 'http://localhost:9998',
+    '127.\\d+.\\d+.\\d+;192.168.\\d+.\\d+;nx-*;t-*;x-*;rsb00*;b000*;kok*-*;*.dev;localhost': 'http://localhost:9990',
+    '172.16.\\d+.\\d+;172.17.\\d+.\\d+;rssv*;*.group': 'http://localhost:9998',
+    //'\\d+.\\d+.\\d+.\\d+': null,
     '*': 'http://localhost:9998'};
 
-  fwd({log:log, servicePort:9999, proxyUrl:'http://localhost:9995', binaryUrl:'http://localhost:9998'});
-  fwd({log:log, servicePort:8888, proxyUrl:'http://localhost:9995', binaryUrl:'http://localhost:9998'});
-  fwdHttp({log:log, servicePort:9995, filters:filters});
+  fwd({log:log, servicePort:9999, proxyUrl:'http://localhost:9991', binaryUrl:'http://localhost:9998'});
+  fwd({log:log, servicePort:8888, proxyUrl:'http://localhost:9992', binaryUrl:'http://localhost:9998'});
+  fwdHttp({log:log, servicePort:9991, filters:filters});
+  fwdHttp({log:log, servicePort:9992, filters:filters});
   fwdHttp({log:log, servicePort:9990}); // direct
   fwdHttp({log:log, servicePort:9998}); // external
