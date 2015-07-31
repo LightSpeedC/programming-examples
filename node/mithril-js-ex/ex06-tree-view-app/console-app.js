804d9e2a02f8109547baa773baa68cefafa4ed42
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-31 20:25:12 +0900

    tree view app

---
+++ node/mithril-js-ex/ex06-tree-view-app/console-app.js
@@ -0,0 +1,34 @@
+this.ConsoleApp = {
+	controller: function (args) {
+		var ctrl = this;
+		ctrl.limit = args && args.limit ? args.limit : 10;
+		ctrl.list = [];
+		ctrl.push = function (msg) {
+			ctrl.list.push({key:Date.now()+':'+Math.random(), msg:msg});
+			if (ctrl.list.length > ctrl.limit) ctrl.list.shift();
+		};
+
+		window.console = function (console) {
+			var c = {};
+			for (var p in console)
+				if (typeof console[p] === 'function')
+					c[p] = function (p, f) {
+						return function () {
+							f.apply(console, arguments);
+							ctrl.push(p + ': ' + [].slice.call(arguments).join(' '));
+							m.redraw(true);
+						}
+					}(p, console[p]);
+			return c;
+		} (console);
+
+		console.log('console.log used');
+	},
+	view: function (ctrl) {
+		return m('pre', [
+			ctrl.list.map(function (item) {
+				return m('div', {key:item.key}, item.msg);
+			})
+		]);
+	}
+};
