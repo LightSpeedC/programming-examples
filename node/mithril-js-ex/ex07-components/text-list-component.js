237933b085f244bd441bb4d9cad6d43a96650376
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-08-05 23:04:00 +0900

    text list console

--- node/mithril-js-ex/ex07-components/text-list-component.js
+++ node/mithril-js-ex/ex07-components/text-list-component.js
@@ -1,14 +1,15 @@
-this.preTextComponent = {
+this.textListComponent = {
 	controller: function (args) {
 		var ctrl = this;
-		ctrl.title = args && args.title ? args.title : 'console';
+		ctrl.title = args && args.title ? args.title : 'text list';
 		ctrl.limit = args && args.limit ? args.limit : 10;
 		ctrl.list = [];
-		ctrl.push = function (msg) {
+		ctrl.log = function (msg) {
 			ctrl.list.push({key:(new Date() - 0) + ':' + Math.random(), msg:msg});
 			if (ctrl.list.length > ctrl.limit) ctrl.list.shift();
 		};
-
+		if (args && args.setLog && typeof args.setLog === 'function')
+			args.setLog(ctrl.log);
 	},
 	view: function (ctrl) {
 		return [
