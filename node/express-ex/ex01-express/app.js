f00384b7090ac990b0bf6c02ef3aa327a6d9c39d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-08-10 06:53:13 +0900

    express example 01

---
+++ node/express-ex/ex01-express/app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+var debug = require('debug')('ex01-express:server');
+
+// respond with "Hello World!" on the homepage
+// respond with "Hello World!" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+app.all('/secret', function (req, res, next) {
+  debug('Accessing the secret section ...');
+  next();  // pass control to the next handler
+});
+
+// public files
+app.use(express.static('public'));
+//app.use('/public', express.static('public'));
+
+// handle error
+app.use(function(req, res, next){
+  res.status(404).send('Sorry cant find that!');
+  //res.send(404, 'Sorry cant find that!');
+});
+
+// handle error
+//app.use(function(err, req, res, next){
+//  console.error(err.stack);
+//  res.send(500, 'Something broke!');
+//});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  if (host === '::') host = 'localhost';
+
+  debug('Example app listening at http://%s:%s', host, port);
+});
