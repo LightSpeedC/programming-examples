36bad8e9f83eeb7e69575c4721760a421e610713
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-08-16 13:03:36 +0900

    revise app.js

--- html/design-mui-ex/app.js
+++ html/design-mui-ex/app.js
@@ -1,2 +1,4 @@
+var port = process.argv[2] || process.env.PORT || 3000;
+console.log('port:', port);
 var express = require('express'), app = express();
-app.use(express.static('.')).listen(process.argv[2] || process.env.PORT || 3000);
+app.use(express.static('.')).listen(port);
