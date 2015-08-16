157cc6bd955e8ba892213485691f1ad4fbc4ac3e
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-08-16 16:07:57 +0900

    revise ctrl and view

--- node/mithril-js-ex/ex08-todo/todo-component.js
+++ node/mithril-js-ex/ex08-todo/todo-component.js
@@ -6,11 +6,11 @@ this.taskListComponent = function () {
 	function Task(task) {
 		this.title = m.prop(task && task.title || '');
 		this.done  = m.prop(task && task.done  || false);
-		this.key = (new Date() - 0) + ':' + Math.random();
+		this.key = (new Date - 0) + Math.random();
+	}
+	Task.prototype.toJSON = function () {
+		return {title: this.title(), done: this.done()};
 	};
-
-	//モデル: TaskListクラスはTaskオブジェクトの配列
-	var TaskList = Array;
 
 	//コントローラ・オブジェクトctrlは
 	//表示されているTaskのリスト(list)を管理し、
@@ -24,165 +24,178 @@ this.taskListComponent = function () {
 		//コントローラは、モデルの中のどの部分が、現在のページと関連するのかを定義している
 		//この場合は1つのコントローラ・オブジェクトctrlですべてを取り仕切っている
 		controller: function (args) {
-			var ctrl = this;
-
 			//アクティブなTaskのリスト
-			ctrl.list = new TaskList();
+			var taskList = [];
+
+			// Taskをタスクリストに追加
+			function addTask(task) {
+				taskList.push(new Task(task));
+			}
+
+			// データのリストからタスクリストに追加
+			function importList(list) {
+				list.forEach(addTask);
+			}
 
 			//引数に渡されたtaskデータリストlistからTaskのリストに追加
-			if (args && args.list) {
-				args.list.forEach(function (task) {
-					ctrl.list.push(new Task(task));
-				});
-			}
+			if (args && args.list)
+				importList(args.list);
 
 			//引数に渡されたtaskデータリストurlからTaskのリストに追加
-			if (args && args.url) {
-				m.request({method: 'GET', url:args.url}).then(function (list) {
-					list.forEach(function (task) {
-						ctrl.list.push(new Task(task));
-					});
+			if (args && args.url)
+				m.request({method: 'GET', url:args.url})
+					.then(importList);
+
+			//新しいTaskを作成する前の、入力中のTaskの名前を保持するスロット
+			var titleInput = m.prop('');
+
+			//Taskをリストに登録し、ユーザが使いやすいようにtitleフィールドをクリアする
+			function addTitleInput() {
+				if (titleInput()) {
+					addTask({title: titleInput()});
+					titleInput('');
+				}
+			}
+
+			//Enterキーに対応
+			function configTitleInput(elem, isInit) {
+				if (isInit) return;
+				elem.focus();
+				elem.onkeypress = function (e) {
+					if ((e || event).keyCode === 13)
+						m_delay(function () { addTitleInput(); elem.focus(); });
+					return true;
+				};
+			}
+
+			//タスクが完了している?
+			function taskIsDone(task) {
+				return task.done();
+			}
+
+			//タスクが完了していない?
+			function taskIsNotDone(task) {
+				return !task.done();
+			}
+
+			//完了タスクの数
+			function countDone() {
+				return taskList.filter(taskIsDone).length;
+			}
+
+			//未了タスクの数
+			function countUndone() {
+				return taskList.filter(taskIsNotDone).length;
+			}
+
+			//完了タスクを全て削除(未了タスクを残す)
+			function removeAllDone() {
+				taskList = taskList.filter(taskIsNotDone);
+			}
+
+			//全て完了/未了
+			function checkAll() {
+				var state = countUndone() !== 0;
+				taskList.forEach(function (task) { task.done(state); });
+			}
+
+			//削除
+			function removeTask(todoToRemove) {
+				taskList = taskList.filter(function (task) {
+					return task !== todoToRemove;
 				});
 			}
 
-			//新しいTaskを作成する前の、入力中のTaskの名前を保持するスロット
-			ctrl.title = m.prop('');
-
-			//Taskをリストに登録し、ユーザが使いやすいようにtitleフィールドをクリアする
-			ctrl.add = function () {
-				if (ctrl.title()) {
-					ctrl.list.push(new Task({title: ctrl.title()}));
-					ctrl.title('');
-				}
-			};
-
-			//Enterキーに対応
-			ctrl.configInput = function (elem, isInit) {
-				elem.focus();
-				if (isInit) return;
-				elem.onkeypress = function (e) {
-					if ((e || event).keyCode === 13)
-						m_delay(function () { ctrl.add(); /*elem.focus();*/ });
-					return true;
-				};
-			};
-
-			//完了タスクの数
-			ctrl.countDone = function () {
-				return ctrl.list.filter(function (task) {
-					return task.done();
-				}).length;
-			};
-
-			//未了タスクの数
-			ctrl.countUndone = function () {
-				return ctrl.list.filter(function (task) {
-					return !task.done();
-				}).length;
-			};
-
-			//完了タスクを全て削除(未了タスクを残す)
-			ctrl.removeAllDone = function () {
-				ctrl.list = ctrl.list.filter(function (task) {
-					return !task.done();
-				});
-			};
-
-			//全て完了/未了
-			ctrl.checkAll = function () {
-				var state = ctrl.countUndone() !== 0;
-				ctrl.list.forEach(function (task) { task.done(state); });
-			};
-
-			//削除
-			ctrl.removeTask = function (todoToRemove) {
-				ctrl.list = ctrl.list.filter(function (task) {
-					return task !== todoToRemove;
-				});
-			};
-
 			//表示モード
-			ctrl.mode = 0; //0:ALL, 1:DONE, 2:UNDONE
+			var dispMode = 0; //0:ALL, 1:DONE, 2:UNDONE
 
 			//編集モード
-			ctrl.taskEdit = undefined;
-			ctrl.toggleEdit = function (task) {
-				if (ctrl.taskEdit) {
-					if (!ctrl.taskEdit.title())
-						ctrl.taskEdit.title(ctrl.saveTitleToEdit);
-					ctrl.taskEdit = undefined;
+			var taskToEdit; // 編集中のTask
+			var saveTitleToEdit; // 編集開始時のタイトル
+			//タスク編集モードをトグル
+			function toggleEditTask(task) {
+				if (taskToEdit) {
+					if (!taskToEdit.title())
+						taskToEdit.title(saveTitleToEdit);
+					taskToEdit = undefined;
 				}
 				else {
-					ctrl.taskEdit = task;
-					if (task)
-						ctrl.saveTitleToEdit = task.title();
+					taskToEdit = task;
+					if (task) saveTitleToEdit = task.title();
 				}
-			};
-			ctrl.configEdit = function (elem, isInit) {
-				elem.focus();
+			}
+			//編集するinputを構成
+			function configEditTask(elem, isInit) {
 				if (isInit) return;
+				elem.focus();
 				elem.onkeypress = function (e) {
 					if ((e || event).keyCode === 13)
-						m_delay(ctrl.toggleEdit);
+						m_delay(toggleEditTask);
 					return true;
 				};
+			}
+
+			this.view = function view(ctrl) {
+				return [
+					m('h1', 'Task管理アプリ'),
+					m('div', ['タスク: ',
+						m('input', m_on('change', 'value', titleInput,
+							{placeholder: '新しいタスクを入力',
+							 config: configTitleInput, autofocus: true})),
+						m('button[type=submit]', {onclick: addTitleInput}, '追加')
+					]),
+					m('hr'),
+					m('div', ['表示: ',
+						m('button[type=button]', {onclick: function () { dispMode = 0; }},
+							[(dispMode === 0 ? '★': '') + '全て', m('span', taskList.length)]),
+						m('button[type=button]', {onclick: function () { dispMode = 2; }},
+							[(dispMode === 2 ? '★': '') + '未了', m('span', countUndone())]),
+						m('button[type=button]', {onclick: function () { dispMode = 1; }},
+							[(dispMode === 1 ? '★': '') + '完了', m('span', countDone())])
+					]),
+					m('div', ['操作: ',
+						m('button[type=button]', {onclick: checkAll}, '全て完了/未了'),
+						m('button[type=button]', {onclick: removeAllDone}, '完了タスクを全て削除')
+					]),
+					m('hr'),
+					m('table', [taskList.map(function(task) {
+						var attrs = {key: task.key};
+						if (dispMode === 1 && !task.done() ||
+							dispMode === 2 &&  task.done())
+								attrs.style = {display: 'none'};
+						return m('tr', attrs,
+							task === taskToEdit ? [
+								//編集
+								m('td', {colspan:2}),
+								m('td', [
+									m('input', m_on('change', 'value', task.title,
+										{onblur: toggleEditTask.bind(null, null), config: configEditTask}))
+								])] : [
+								//表示
+								m('td', [
+									m('button[type=reset]', {onclick: removeTask.bind(null, task)}, '削除')
+								]),
+								m('td', [
+									m('input[type=checkbox]', m_on('click', 'checked', task.done))
+								]),
+								m('td',
+									{style: {textDecoration: task.done() ? 'line-through' : 'none'},
+									 ondblclick: toggleEditTask.bind(null, task)},
+									task.title())]
+						);
+					})]),
+					m('div', '※ダブルクリックで編集')
+					, m('pre', JSON.stringify(taskList, null, '  ').split('\n').map(function (x) {
+						return m('div', x);
+					}))
+				];
 			};
 
 		},
 
 		//ビュー
 		view: function (ctrl) {
-			return [
-				m('h1', 'Task管理アプリ'),
-				m('div', ['タスク: ',
-					m('input', m_on('change', 'value', ctrl.title,
-						{autofocus: true,
-						 placeholder: '新しいタスクを入力',
-						 config: ctrl.configInput})),
-					m('button[type=submit]', {onclick: ctrl.add}, '追加')
-				]),
-				m('hr'),
-				m('div', ['表示: ',
-					m('button[type=button]', {onclick: function () { ctrl.mode = 0; }},
-						[(ctrl.mode === 0 ? '★': '') + '全て', m('span', ctrl.list.length)]),
-					m('button[type=button]', {onclick: function () { ctrl.mode = 2; }},
-						[(ctrl.mode === 2 ? '★': '') + '未了', m('span', ctrl.countUndone())]),
-					m('button[type=button]', {onclick: function () { ctrl.mode = 1; }},
-						[(ctrl.mode === 1 ? '★': '') + '完了', m('span', ctrl.countDone())])
-				]),
-				m('div', ['操作: ',
-					m('button[type=button]', {onclick: ctrl.checkAll}, '全て完了/未了'),
-					m('button[type=button]', {onclick: ctrl.removeAllDone}, '完了タスクを全て削除')
-				]),
-				m('hr'),
-				m('table', [ctrl.list.map(function(task) {
-					var attrs = {key: task.key};
-					if (ctrl.mode === 1 && !task.done() ||
-						ctrl.mode === 2 &&  task.done())
-							attrs.style = {display: 'none'};
-					return m('tr', attrs,
-						task === ctrl.taskEdit ? [
-							//編集
-							m('td', {colspan:2}),
-							m('td', [
-								m('input', m_on('change', 'value', task.title,
-									{onblur: ctrl.toggleEdit.bind(ctrl, null), config: ctrl.configEdit}))
-							])] : [
-							//表示
-							m('td', [
-								m('button[type=reset]', {onclick: ctrl.removeTask.bind(null, task)}, '削除')
-							]),
-							m('td', [
-								m('input[type=checkbox]', m_on('click', 'checked', task.done))
-							]),
-							m('td',
-								{style: {textDecoration: task.done() ? 'line-through' : 'none'},
-								 ondblclick: ctrl.toggleEdit.bind(ctrl, task)},
-								task.title())]
-					);
-				})]),
-				m('div', '※ダブルクリックで編集')
-			];
+			return ctrl.view(ctrl);
 		}
 	};
 
