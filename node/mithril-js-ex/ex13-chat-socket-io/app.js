21a55864beef04d7f360b6a5027106880b7be4a7
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-09 07:26:52 +0900

    chat socket io jquery

--- node/mithril-js-ex/ex13-chat-socket-io/app.js
+++ node/mithril-js-ex/ex13-chat-socket-io/app.js
@@ -2,19 +2,23 @@ var port = process.argv[2] || process.env.PORT || 3000;
 console.log('port:', port);
 var express = require('express'), app = express();
 var http = require('http'), server = http.createServer(app).listen(port);
-var io = require('socket.io')(server);
 app.use(express.static('.'));
+var io = require('socket.io')(server);
 // app.get('/', function(req, res){
 //	res.sendfile('index.html');
 // });
-
+var list = [{system: 'msg1'}, {system:'msg2'}];
 io.on('connection', function(socket){
-	console.log('a user connected');
+	//console.log('a user connected');
+	socket.emit('all messages', list);
 	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
+		//console.log('message: ' + msg);
 		io.emit('chat message', msg);
+		list.push(msg);
+		if (list.length > 10)
+			list.shift();
 	});
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		//console.log('user disconnected');
 	});
 });
