0f128b87408916de18daeaf4c534999d10570d2b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-19 09:50:43 +0900

    space2tab

---
+++ node/add-space-ex/space2tab.js
@@ -0,0 +1,18 @@
+	'use strict';
+	var inFileName = process.argv[2];
+	var outFileName = process.argv[3] || (inFileName + '.log');
+	var numSpaces = Number(process.argv[4] || 2);
+	if (!inFileName) return console.log('node space2tab {in} {out=in.log} {numSpaces=2}');
+	var fs = require('fs');
+	var lines = fs.readFileSync(inFileName).toString().split('\r\n');
+	lines = lines.map(function (line) {
+		if (line.length < numSpaces) return line;
+		for (var i = 0; i < line.length; ++i)
+			if (line.charAt(i) !== ' ') break;
+		if (i < line.length)
+			return [].map.call(line.substr(0, i / numSpaces),
+				function () { return '\t'; }).join('') +
+				line.substr(Math.floor(i / numSpaces) * numSpaces);
+		return line;
+	});
+	fs.writeFileSync(outFileName, lines.join('\r\n'));
