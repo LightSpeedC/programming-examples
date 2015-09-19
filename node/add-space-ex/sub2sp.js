6676d2e93e43baa431328d086ff6daa46be1b25d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-19 09:50:24 +0900

    default output file name

--- node/add-space-ex/sub2sp.js
+++ node/add-space-ex/sub2sp.js
@@ -1,8 +1,7 @@
   'use strict';
   var inFileName = process.argv[2];
-  var outFileName = process.argv[3];
+  var outFileName = process.argv[3] || (inFileName + '.log');
   if (!inFileName) return console.log('node sub2sp {in} {out}');
-  if (!outFileName) outFileName = inFileName + '.log';
   var fs = require('fs');
   var lines = fs.readFileSync(inFileName).toString().split('\r\n');
   lines = lines.map(function (line) {
