04fb95f3a8f37520ae9ff6eea4d193438f6cb9f6
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-19 09:11:59 +0900

    codestyle format

--- node/add-space-ex/sub2sp.js
+++ node/add-space-ex/sub2sp.js
@@ -1,10 +1,11 @@
-  inFileName=process.argv[2]
-  outFileName=process.argv[3]
-  if (!inFileName) return console.log('node sub2sp {in} {out}')
+  'use strict';
+  var inFileName = process.argv[2];
+  var outFileName = process.argv[3];
+  if (!inFileName) return console.log('node sub2sp {in} {out}');
   if (!outFileName) outFileName = inFileName + '.log';
-  fs=require('fs')
-  lines=fs.readFileSync(inFileName).toString().split('\r\n');
-  lines=lines.map(function(line){
+  var fs = require('fs');
+  var lines = fs.readFileSync(inFileName).toString().split('\r\n');
+  lines = lines.map(function (line) {
     if (line.length < 2) return line;
     if (line.substr(0, 2) === '  ') return line.substr(2);
     return line;
