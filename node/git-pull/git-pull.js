e8ca821ab4d142b9071c87a08ec4914f85bc166a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-26 17:22:49 +0900

    git pull

---
+++ node/git-pull/git-pull.js
@@ -0,0 +1,29 @@
+(function () {
+	'use strict';
+
+	var aa = require('aa');
+	var fs = require('fs');
+	var path = require('path');
+	var fs_readdir = aa(fs.readdir);
+	var fs_stat = aa(fs.stat);
+
+	aa(tree(path.resolve('.')));
+
+	function* tree(dir) {
+		console.log(':', dir);
+		var names = yield fs_readdir(dir);
+		if (names.indexOf('.git') >= 0) return;
+		//console.log(names);
+		yield names.map(name => function *() {
+			//console.log(name);
+			var file = path.resolve(dir, name);
+			var stat = yield fs_stat(file);
+			if (stat.isDirectory()) {
+				console.log(file + '/');
+				yield tree(path.resolve(dir, name));
+			}
+			//console.log(stat);
+		});
+	}
+
+})();
