33507245fbfe085edeada2716874346f8b54988d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-27 06:34:40 +0900

    log -> info, error

--- node/es6-ex/es6-check.js
+++ node/es6-ex/es6-check.js
@@ -2,43 +2,43 @@
 
 try {
 	eval('(function*() { yield 1 })()');
-	console.log('%c%s', 'color:green', 'yield を使えます');
+	console.info('%c%s', 'color:green', 'yield を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'yield は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'yield は使えません') }
 
 
 try {
 	eval('(function(a=10) { return a })()');
-	console.log('%c%s', 'color:green', 'デフォルト引数 を使えます');
+	console.info('%c%s', 'color:green', 'デフォルト引数 を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'デフォルト引数 は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'デフォルト引数 は使えません') }
 
 try {
 	eval('(function(...numbers) { return numbers })()');
-	console.log('%c%s', 'color:green', '可変引数 を使えます');
+	console.info('%c%s', 'color:green', '可変引数 を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', '可変引数 は使えません') }
+catch (e) { console.error('%c%s', 'color:red', '可変引数 は使えません') }
 
 try {
 	eval('(()=>{ return null })();');
-	console.log('%c%s', 'color:green', 'アローファンクション を使えます');
+	console.info('%c%s', 'color:green', 'アローファンクション を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'アローファンクション は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'アローファンクション は使えません') }
 
 try {
 	eval('(function() { let foo = 1 })()');
-	console.log('%c%s', 'color:green', 'let を使えます');
+	console.info('%c%s', 'color:green', 'let を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'let は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'let は使えません') }
 
 try {
 	eval('(function() { const foo = 1 })()');
-	console.log('%c%s', 'color:green', 'const を使えます');
+	console.info('%c%s', 'color:green', 'const を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'const は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'const は使えません') }
 
 try {
 	eval('(function() { var myMap = new Map(); })()');
-	console.log('%c%s', 'color:green', 'Map を使えます');
+	console.info('%c%s', 'color:green', 'Map を使えます');
 } 
-catch (e) { console.log('%c%s', 'color:red', 'Map は使えません') }
+catch (e) { console.error('%c%s', 'color:red', 'Map は使えません') }
