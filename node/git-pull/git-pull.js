a311b1876c09d67a37e243ac9f801a597f6a6dc7
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-26 17:54:38 +0900

    revise git pull

--- node/git-pull/git-pull.js
+++ node/git-pull/git-pull.js
@@ -7,19 +7,24 @@
 	var fs_readdir = aa(fs.readdir);
 	var fs_stat = aa(fs.stat);
 
-	aa(tree(path.resolve('.')));
+	aa(tree(path.resolve(process.argv[2] || '.')));
 
 	function* tree(dir) {
-		console.log(':', dir);
+		//console.log(':', dir);
 		var names = yield fs_readdir(dir);
-		if (names.indexOf('.git') >= 0) return;
+		if (names.indexOf('.git') >= 0) {
+			console.log('Git ===> ', dir);
+			//console.log(dir, names);
+			return;
+		}
 		//console.log(names);
 		yield names.map(name => function *() {
 			//console.log(name);
+			if (name === 'node_modules') return;
 			var file = path.resolve(dir, name);
 			var stat = yield fs_stat(file);
 			if (stat.isDirectory()) {
-				console.log(file + '/');
+				//console.log(file + '/');
 				yield tree(path.resolve(dir, name));
 			}
 			//console.log(stat);
