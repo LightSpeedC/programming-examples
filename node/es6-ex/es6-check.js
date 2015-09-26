397e69661fe93f28716fbb364802dc9db47870c7
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-27 06:32:58 +0900

    remove * asterisk

--- node/es6-ex/es6-check.js
+++ node/es6-ex/es6-check.js
@@ -8,13 +8,13 @@ catch (e) { console.log('%c%s', 'color:red', 'yield は使えません') }
 
 
 try {
-	eval('(function*(a=10) { return a })()');
+	eval('(function(a=10) { return a })()');
 	console.log('%c%s', 'color:green', 'デフォルト引数 を使えます');
 } 
 catch (e) { console.log('%c%s', 'color:red', 'デフォルト引数 は使えません') }
 
 try {
-	eval('(function*(...numbers) { return numbeers })()');
+	eval('(function(...numbers) { return numbeers })()');
 	console.log('%c%s', 'color:green', '可変引数 を使えます');
 } 
 catch (e) { console.log('%c%s', 'color:red', '可変引数 は使えません') }
@@ -26,19 +26,19 @@ try {
 catch (e) { console.log('%c%s', 'color:red', 'アローファンクション は使えません') }
 
 try {
-	eval('(function*() { let foo = 1 })()');
+	eval('(function() { let foo = 1 })()');
 	console.log('%c%s', 'color:green', 'let を使えます');
 } 
 catch (e) { console.log('%c%s', 'color:red', 'let は使えません') }
 
 try {
-	eval('(function*() { const foo = 1 })()');
+	eval('(function() { const foo = 1 })()');
 	console.log('%c%s', 'color:green', 'const を使えます');
 } 
 catch (e) { console.log('%c%s', 'color:red', 'const は使えません') }
 
 try {
-	eval('(function*() { var myMap = new Map(); })()');
+	eval('(function() { var myMap = new Map(); })()');
 	console.log('%c%s', 'color:green', 'Map を使えます');
 } 
 catch (e) { console.log('%c%s', 'color:red', 'Map は使えません') }
