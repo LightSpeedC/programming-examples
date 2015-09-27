e9daef3de912769e550fcd45bc25a8b1d32a060a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-27 11:51:37 +0900

    revise and to english

--- node/es6-ex/es6-check.js
+++ node/es6-ex/es6-check.js
@@ -1,44 +1,43 @@
 // http://qiita.com/_shimizu/items/1dd5dd0f9f88f96188ef
 
 try {
-	eval('(function*() { yield 1 })()');
-	console.info('%c%s', 'color:green', 'yield を使えます');
+	eval('(function*() { yield 101 })().next().value');
+	console.info('o yield enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'yield は使えません') }
-
+catch (e) { console.error('x yield disabled:', e) }
 
 try {
 	eval('(function(a=10) { return a })()');
-	console.info('%c%s', 'color:green', 'デフォルト引数 を使えます');
+	console.info('o default arguments enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'デフォルト引数 は使えません') }
+catch (e) { console.error('x default arguments disabled:', e) }
 
 try {
-	eval('(function(...numbers) { return numbers })()');
-	console.info('%c%s', 'color:green', '可変引数 を使えます');
+	eval('(function(...args) { return args; })()');
+	console.info('o variable arguments/rest arguments enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', '可変引数 は使えません') }
+catch (e) { console.error('x variable arguments/rest arguments disabled:', e) }
 
 try {
 	eval('(()=>{ return null })();');
-	console.info('%c%s', 'color:green', 'アローファンクション を使えます');
+	console.info('o arrow function enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'アローファンクション は使えません') }
+catch (e) { console.error('x arrow function disabled:', e) }
 
 try {
-	eval('(function() { let foo = 1 })()');
-	console.info('%c%s', 'color:green', 'let を使えます');
+	eval('(function() { "use strict"; var foor = 2; {let foo = 1; return foo; } })()');
+	console.info('o let enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'let は使えません') }
+catch (e) { console.error('x let disabled:', e) }
 
 try {
-	eval('(function() { const foo = 1 })()');
-	console.info('%c%s', 'color:green', 'const を使えます');
+	eval('(function() { "use strict"; const foo = 1; })()');
+	console.info('o const enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'const は使えません') }
+catch (e) { console.error('x const disabled:', e) }
 
 try {
 	eval('(function() { var myMap = new Map(); })()');
-	console.info('%c%s', 'color:green', 'Map を使えます');
+	console.info('o Map enabled');
 } 
-catch (e) { console.error('%c%s', 'color:red', 'Map は使えません') }
+catch (e) { console.error('x Map disabled:', e) }
