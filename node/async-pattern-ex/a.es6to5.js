6e50b67a055331786133c5e39950cb0e44f5e560
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-30 01:09:45 +0900

    rename

--- node/async-pattern-ex/a.es6to5.js
+++ node/async-pattern-ex/a.es6to5.js
@@ -29,12 +29,12 @@ function g() {
 var gg = g();
 console.log(typeof g, g.constructor.name);
 console.log(typeof gg, gg.constructor.name);
-console.log(gg.__proto__.constructor.name);
-console.log(gg.__proto__.__proto__.constructor.name);
-console.log(gg.__proto__.__proto__.__proto__.constructor.name);
-console.log(gg);
-console.log(gg.next());
-console.log(gg.next());
-console.log(gg.next());
-console.log(gg.next());
+//console.log(gg.__proto__.constructor.name);
+//console.log(gg.__proto__.__proto__.constructor.name);
+//console.log(gg.__proto__.__proto__.__proto__.constructor.name);
+console.dir(gg);
+console.dir(gg.next());
+console.dir(gg.next());
+console.dir(gg.next());
+console.dir(gg.next());
 
