89f7d368ff38327d38a6083bad2ebe767847d2dc
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-09-30 01:54:35 +0900

    build script

---
+++ node/async-pattern-ex/build.cmd
@@ -0,0 +1,5 @@
+call babel a.js > a.es6to5.js
+call babel b.js > b.es6to5.js
+call babel a3-aa-yield-promise.js > a3-aa-yield-promise.es6to5.js
+call babel a4-aa-yield-chan.js    > a4-aa-yield-chan.es6to5.js
+pause
