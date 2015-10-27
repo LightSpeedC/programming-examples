c134d64902d845eca2af23f7f642f89138db86e2
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-10-28 07:03:12 +0900

    space to tab

--- node/postgresql-ex/ex01/ex01a-pg-client-pooling.js
+++ node/postgresql-ex/ex01/ex01a-pg-client-pooling.js
@@ -1,34 +1,34 @@
 // http://nodejs.osser.jp/node/node-postgres/
 // https://github.com/brianc/node-postgres
 
-// Client pooling
-var pg = require('pg');
+	// Client pooling
+	var pg = require('pg');
 
-var host = process.env.PGHOST     || 'localhost';
-var port = process.env.PGPORT     || '5432';
-var db   = process.env.PGDATABASE || 'postgres';
-var user = process.env.PGUSER     || 'postgres';
-var pw   = process.env.PGPASSWORD || 'password';
+	var host = process.env.PGHOST     || 'localhost';
+	var port = process.env.PGPORT     || '5432';
+	var db   = process.env.PGDATABASE || 'postgres';
+	var user = process.env.PGUSER     || 'postgres';
+	var pw   = process.env.PGPASSWORD || 'password';
 
-var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
+	var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
 
-//this starts initializes a connection pool
-//it will keep idle connections open for a (configurable) 30 seconds
-//and set a limit of 20 (also configurable)
-pg.connect(conString, function (err, client, done) {
-  if (err)
-    return console.error('error fetching client from pool', err);
+	//this starts initializes a connection pool
+	//it will keep idle connections open for a (configurable) 30 seconds
+	//and set a limit of 20 (also configurable)
+	pg.connect(conString, function (err, client, done) {
+		if (err)
+			return console.error('error fetching client from pool', err);
 
-  client.query('SELECT $1::int AS num', ['1'], function (err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-    if (err)
-      return console.error('error running query', err);
+		client.query('SELECT $1::int AS num', ['1'], function (err, result) {
+			//call `done()` to release the client back to the pool
+			done();
+			if (err)
+				return console.error('error running query', err);
 
-    console.log(result.rows[0].num);
-    //output: 1
+			console.log(result.rows[0].num);
+			//output: 1
 
-    // end/disconnect
-    pg.end();
-  });
-});
+			// end/disconnect
+			pg.end();
+		});
+	});
--- node/postgresql-ex/ex01/ex01b-pg-client-instance.js
+++ node/postgresql-ex/ex01/ex01b-pg-client-instance.js
@@ -1,29 +1,29 @@
 // http://nodejs.osser.jp/node/node-postgres/
 // https://github.com/brianc/node-postgres
 
-// Client instance
-var pg = require('pg');
+	// Client instance
+	var pg = require('pg');
 
-var host = process.env.PGHOST     || 'localhost';
-var port = process.env.PGPORT     || '5432';
-var db   = process.env.PGDATABASE || 'postgres';
-var user = process.env.PGUSER     || 'postgres';
-var pw   = process.env.PGPASSWORD || 'password';
+	var host = process.env.PGHOST     || 'localhost';
+	var port = process.env.PGPORT     || '5432';
+	var db   = process.env.PGDATABASE || 'postgres';
+	var user = process.env.PGUSER     || 'postgres';
+	var pw   = process.env.PGPASSWORD || 'password';
 
-var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
+	var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err)
-    return console.error('could not connect to postgres', err);
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+		if(err)
+			return console.error('could not connect to postgres', err);
 
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err)
-      return console.error('error running query', err);
+		client.query('SELECT NOW() AS "theTime"', function(err, result) {
+			if(err)
+				return console.error('error running query', err);
 
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+			console.log(result.rows[0].theTime);
+			//output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
 
-    client.end();
-  });
-});
+			client.end();
+		});
+	});
