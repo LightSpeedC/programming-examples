5dfea38e08128ce517de155cdc19f25af6c0c58b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-10-28 09:21:37 +0900

    pg aa promisify example

---
+++ node/postgresql-ex/ex05-aa-promisify/ex05b-pg-client-instance.js
@@ -0,0 +1,62 @@
+(function () {
+	'use strict';
+
+	var pg = require('pg');
+	var aa = require('aa'), promisify = aa.promisify, Channel = aa.Channel;
+
+	// Client instance
+
+	var host = process.env.PGHOST     || 'localhost';
+	var port = process.env.PGPORT     || '5432';
+	var db   = process.env.PGDATABASE || 'postgres';
+	var user = process.env.PGUSER     || 'postgres';
+	var pw   = process.env.PGPASSWORD || 'password';
+
+	var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
+	//pg.connectA = promisify.call(pg, pg.connect);
+
+	aa(function *() {
+
+		var client = new pg.Client(conString);
+		client.connectA = promisify.call(client, client.connect);
+		client.queryA = promisify.call(client, client.query);
+
+		try {
+			var msg = 'could not connect to postgres';
+			yield client.connectA();
+
+			console.log('connected!!!');
+
+			//client.queryA('SELECT $1::int AS numbor', ['1'])
+			msg = 'error running query';
+			var result = yield client.queryA('select * from s_tenant_r', []);
+
+			//console.log(result.rows[0]);
+			console.log(result);
+
+			console.log('client.end()++');
+			client.end();
+			//yield promisify.call(client, client.end);
+			console.log('client.end()--');
+		}
+		catch (err) {
+			return console.error(msg, err);
+		}
+
+		yield aa.wait(100);
+
+		var client = new pg.Client(conString);
+		client.connectA = promisify.call(client, client.connect);
+		client.queryA = promisify.call(client, client.query);
+
+		console.log('2nd connectA++');
+		yield client.connectA();
+		console.log('2nd connectA--');
+		var result = yield client.queryA('select * from s_tenant_r', []);
+		console.log(result);
+		client.end();
+		yield aa.wait(100);
+		//console.log('process.exit()');
+		//process.exit();
+	});
+})();
