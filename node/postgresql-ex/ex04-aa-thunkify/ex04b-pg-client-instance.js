466998e02bdfb5e5fc02129e9861feb185cbaddf
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-10-30 21:58:54 +0900

    copy

---
+++ node/postgresql-ex/ex04-aa-thunkify/ex04b-pg-client-instance.js
@@ -0,0 +1,57 @@
+(function () {
+	'use strict';
+
+	var pg = require('pg');
+	var aa = require('aa');
+
+	aa.thunkifyAll(pg.constructor.prototype, {postfix: 'A'});
+	aa.thunkifyAll(pg.Client.prototype, {postfix: 'A'});
+
+	// Client instance
+
+	var host = process.env.PGHOST     || 'localhost';
+	var port = process.env.PGPORT     || '5432';
+	var db   = process.env.PGDATABASE || 'postgres';
+	var user = process.env.PGUSER     || 'postgres';
+	var pw   = process.env.PGPASSWORD || 'password';
+
+	var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
+
+	aa(function *() {
+
+		var client = new pg.Client(conString);
+
+		try {
+			var msg = 'could not connect to postgres';
+			yield client.connectA();
+
+			console.log('connected!!!');
+
+			//client.queryA('SELECT $1::int AS numbor', ['1'])
+			msg = 'error running query';
+			var result = yield client.queryA('select * from s_tenant_r', []);
+
+			//console.log(result.rows[0]);
+			console.log(result);
+
+			console.log('client.end()++');
+			client.end();
+			console.log('client.end()--');
+		}
+		catch (err) {
+			return console.error(msg, err);
+		}
+
+		yield aa.wait(100);
+
+		var client = new pg.Client(conString);
+
+		console.log('2nd connectA++');
+		yield client.connectA();
+		console.log('2nd connectA--');
+		var result = yield client.queryA('select * from s_tenant_r', []);
+		console.log(result);
+		client.end();
+		yield aa.wait(100);
+	});
+})();
---
+++ node/postgresql-ex/ex05-aa-promisify/ex05a-pg-client-pooling.js
@@ -0,0 +1,49 @@
+(function () {
+	'use strict';
+
+	var pg = require('pg');
+	var aa = require('aa');
+
+	aa.promisifyAll(pg.constructor.prototype, {postfix: 'A'});
+	aa.promisifyAll(pg.Client.prototype, {postfix: 'A'});
+
+	// Client pooling
+
+	var host = process.env.PGHOST     || 'localhost';
+	var port = process.env.PGPORT     || '5432';
+	var db   = process.env.PGDATABASE || 'postgres';
+	var user = process.env.PGUSER     || 'postgres';
+	var pw   = process.env.PGPASSWORD || 'password';
+
+	var conString = 'postgres://' + user + ':' + pw + '@' + host + '/' + db;
+
+	aa(function *() {
+		try {
+			var msg = 'error fetching client from pool';
+
+			var result = yield pg.connectA(conString);
+			var client = result[0], done = result[1];
+
+			console.log('connected!!!');
+
+			msg = 'error running query';
+
+			//client_query('SELECT $1::int AS numbor', ['1'])
+			var result = yield client.queryA('select * from s_tenant_r', []);
+
+			//call `done()` to release the client back to the pool
+			done();
+
+			//console.log(result.rows[0]);
+			console.log(result);
+
+			console.log('end');
+
+		}
+		catch (err) {
+			return console.error(msg, err);
+		}
+
+		pg.end();
+	});
+})();
