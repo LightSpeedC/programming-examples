5132667376317f5dc282535b5689a3eb5c3d842b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-11-02 21:17:49 +0900

    revise nvm

--- node/nvm/nvm.cmd
+++ node/nvm/nvm.cmd
@@ -1,6 +1,10 @@
 @set HERE=%~dp0
+@pushd %HERE%
+@if not exist node_modules call npm install
+@if not exist nodex.exe copy ..\nodejs\node.exe nodex.exe
 @echo "" > %HERE%nvm_out.cmd
 @%HERE%nodex %HERE%nvm.js %*
 @node -v
 @call %HERE%nvm_out
 @del %HERE%nvm_out.cmd
+@popd
