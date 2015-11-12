3703e1e77faffd539608dd3c95d2342a5115ab59
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-11-13 07:28:36 +0900

    tuning

--- node/promise-then-ex/promise-then-ex.js
+++ node/promise-then-ex/promise-then-ex.js
@@ -12,11 +12,20 @@
 		});
 	}
 
-	sleep(1000, 'a')
-	.then((v) => {console.log(v); return sleep(1000, 'b'); })
-	.then((v) => {console.log(v); throw new Error('xxx'); return sleep(1000, 'c'); })
-	.then(console.log)
-	.catch((e) => console.log('catch all: ' + e));
+	if (typeof sleep(0, 'test') === 'function') {
+		console.log('thunkified promise ***************************************************************');
+		sleep(1000, 'a')
+		((e, v) => {console.log(e ? 'a error: ' + e : v); throw new Error('xxx'); return sleep(1000, 'b'); })
+		((e, v) => {console.log(e ? 'b error: ' + e : v); return e ? e : sleep(1000, 'c'); })
+		((e, v) => console.log(e ? 'catch all: ' + e : v));
+	}
+	else {
+		sleep(1000, 'a')
+		.then((v) => {console.log(v); return sleep(1000, 'b'); })
+		.then((v) => {console.log(v); throw new Error('xxx'); return sleep(1000, 'c'); })
+		.then(console.log)
+		.catch((e) => console.log('catch all: ' + e));
+	}
 
 	console.log(sleep(1000, 'a'));
 	console.log('pending: %s', sleep(1000, 'a'));
