a7390b8926eaf9f85e80b1af6cec2b6247118f13
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-03 07:34:25 +0900

    revise

--- node/promise-from-thunk-ex/promise-with-thunk-ex.js
+++ node/promise-from-thunk-ex/promise-with-thunk-ex.js
@@ -4,12 +4,19 @@ function sleep(ms, val, cb) {
 	setTimeout(cb, ms, null, val);
 }
 
-var wait = promiseFromThunk(delay);
+//var wait = promiseFromThunk(delay);
+var wait = promisify(sleep);
 
 console.log('start');
 wait(1000, 'a1')
-.then(val => (console.log(val), wait(1000, 'b2')))
-.then(val => (console.log(val), wait(1000, 'c3')))
+.then(val => (console.log(val), wait(1000, 'b1')))
+.then(val => (console.log(val), wait(1000, 'c1')))
+.then(console.log);
+
+var x = wait(4000, 'a2')
+(function (err, val) { console.log(val); return wait(1000, 'b2'); })
+(function (err, val) { console.log(val); return wait(1000, 'c2'); })
+console.log(typeof x, x && x.constructor.name);
 
 function thunkify(fn) {
 	return function () {
@@ -22,14 +29,46 @@ function thunkify(fn) {
 		return function (callback) {
 			cb = callback;
 			if (result) cb.apply(ctx, result);
+			return 222;
 		};
 	};
 }
 
-function delay(ms, val) {
-	return function (cb) {
-		setTimeout(cb, ms, null, val);
+function promisify(fn) {
+	return function () {
+		var ctx = this, args = arguments, cb, result, resolve, reject;
+		args[args.length++] = function (err, val) {
+			result = arguments;
+			if (cb) cb.apply(ctx, result);
+			if (err) reject(err);
+			else resolve(val);
+		};
+		fn.apply(ctx, args);
+		var p = new Promise(function (res, rej) {
+			resolve = res; reject = rej;
+		});
+		thunk.then = p.then.bind(p);
+		thunk['catch'] = p['catch'].bind(p);
+		return thunk;
+		function thunk(callback) {
+			cb = function () {
+				var r = callback.apply(this, arguments);
+				console.log(444, 'r', typeof r, r && r.constructor.name);
+				console.log(555, 'then', r && typeof r.then);
+				//if (r && typeof r.then === 'function') return r.then(console.log, console.log);
+				if (typeof r === 'function') return r(console.log);
+				//if (r && typeof r.then === 'function')
+				//	r.then
+				return r;
+			};
+			if (result) cb.apply(ctx, result);
+			return thunk;
+		}
 	};
+}
+
+function delay(ms, val) {
+	return cb => setTimeout(cb, ms, null, val);
 }
 
 function promiseFromThunk(thunk) {
