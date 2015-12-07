aacbe99b12dbe1f158a23c1598a17c5a83c914d3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-08 07:17:34 +0900

    refactor - extract dumpBuffer

---
+++ node/tcp-log-jnet/dump-buffer.js
@@ -0,0 +1,44 @@
+void function () {
+	'use strict';
+
+	module.exports = dumpBuffer;
+
+	var N = 16;
+	function dumpBuffer(buf, dir) {
+		if (!buf) return;
+
+		if (buf.length === 0) return wr('Buffer <>\r\n');
+
+		for (var j = 0, n = buf.length; j < n; j += N) {
+			wr(dir + ('000' + j.toString(16)).slice(-4) + ': ');
+			for (var i = 0; i < N; ++i) {
+				var c = i + j < n ? buf[i + j] : '.'.charCodeAt(0);
+				var s = ('0' + c.toString(16)).slice(-2);
+				if (i + j < n)
+					if (c >= 0x20 && c <= 0x7e) wr(' ' + s);
+					else if (c === 0x0d) wr(' \x1b[41m' + s + '\x1b[m');
+					else if (c === 0x0a) wr(' \x1b[42m' + s + '\x1b[m');
+					else wr(' \x1b[43m' + s + '\x1b[m');
+				else wr(' \x1b[44;30m**\x1b[m');
+			}
+			wr('  ');
+			for (var i = 0; i < N; ++i) {
+				var c = i + j < n ? buf[i + j] : '.'.charCodeAt(0);
+				if (i + j < n)
+					if (c >= 0x20 && c <= 0x7e) wr(String.fromCharCode(c));
+					else if (c === 0x0d) wr('\x1b[41mr\x1b[m');
+					else if (c === 0x0a) wr('\x1b[42mn\x1b[m');
+					else wr('\x1b[43m*\x1b[m');
+				else wr('\x1b[44;30m*\x1b[m');
+			}
+			wr('  **\r\n');
+		}
+
+	}
+
+	function wr(x) {
+		process.stdout.write(x);
+	}
+
+	dumpBuffer.wr = wr;
+}();
--- node/tcp-log-jnet/test1.js
+++ node/tcp-log-jnet/test1.js
@@ -9,6 +9,8 @@ void function () {
 
 	var settings = require('./settings.json');
 
+	var dumpBuffer = require('./dump-buffer');
+
 	var port = 3000;
 	var net = require('net');
 	var server = net.createServer(soc => {
@@ -23,7 +25,7 @@ void function () {
 			if (!buf) return;
 			//console.log('read', buf);
 			soc.write(buf);
-			dump(buf, 'xxx ');
+			dumpBuffer(buf, 'xxx ');
 		});
 		soc.on('end', () => {
 			console.log('end');
@@ -32,42 +34,5 @@ void function () {
 		soc.on('error', err => { console.log('err', err); });
 		//soc.write()
 	}).listen(port, () => console.log(port + ' started'));
-
-	var N = 16;
-	function dump(buf, dir) {
-		if (!buf) return;
-
-		if (buf.length === 0) return wr('Buffer <>\r\n');
-
-		for (var j = 0, n = buf.length; j < n; j += N) {
-			wr(dir + ('000' + j.toString(16)).slice(-4) + ': ');
-			for (var i = 0; i < N; ++i) {
-				var c = i + j < n ? buf[i + j] : '.'.charCodeAt(0);
-				var s = ('0' + c.toString(16)).slice(-2);
-				if (i + j < n)
-					if (c >= 0x20 && c <= 0x7e) wr(' ' + s);
-					else if (c === 0x0d) wr(' \x1b[41m' + s + '\x1b[m');
-					else if (c === 0x0a) wr(' \x1b[42m' + s + '\x1b[m');
-					else wr(' \x1b[43m' + s + '\x1b[m');
-				else wr(' \x1b[44;30m**\x1b[m');
-			}
-			wr('  ');
-			for (var i = 0; i < N; ++i) {
-				var c = i + j < n ? buf[i + j] : '.'.charCodeAt(0);
-				if (i + j < n)
-					if (c >= 0x20 && c <= 0x7e) wr(String.fromCharCode(c));
-					else if (c === 0x0d) wr('\x1b[41mr\x1b[m');
-					else if (c === 0x0a) wr('\x1b[42mn\x1b[m');
-					else wr('\x1b[43m*\x1b[m');
-				else wr('\x1b[44;30m*\x1b[m');
-			}
-			wr('  **\r\n');
-		}
-
-	}
-
-	function wr(x) {
-		process.stdout.write(x);
-	}
 
 }();
