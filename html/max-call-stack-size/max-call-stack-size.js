0f317e2b91d1f0c3b7fbdb0bd69d2777318d95d0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-14 01:37:02 +0900

    max call stack size

---
+++ html/max-call-stack-size/max-call-stack-size.js
@@ -0,0 +1,12 @@
+function recursiveFunction(i) {
+	try {
+		var y = recursiveFunction(i + 1);
+	} catch (err) { err.count = i; return err; }
+	return y;
+}
+
+console.info(typeof navigator !== 'undefined' && navigator.userAgent || 'Node.js');
+var err = recursiveFunction(0);
+var keys = (Object.getOwnPropertyNames || Object.keys)(err);
+for (var i in keys)
+	console.warn(keys[i] + ': ' + err[keys[i]]);
