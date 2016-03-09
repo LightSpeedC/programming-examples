ca33131411e90fd9d9e839f6f6dcdbd9c5bbdbac
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-03-09 14:13:17 +0900

    generate random password

--- node/random-password-ex/random-password-ex.js
+++ node/random-password-ex/random-password-ex.js
@@ -11,10 +11,25 @@
 	var len = seed.length;
 
 	function genPwd(n) {
-		var pwd = '';
-		for (var i = 0; i < n; ++i)
-			pwd += seed[Math.floor(Math.random()*len)];
+		do {
+			var pwd = '';
+			for (var i = 0; i < n; ++i)
+				pwd += seed[Math.floor(Math.random()*len)];
+			//console.log(pwd, 'ok?');
+		} while (!checkPwd(pwd));
+		//console.log(pwd, 'ok!');
 		return pwd;
+	}
+
+	function checkPwd(pwd) {
+		if (pwd.length < n) return false;
+		if (!pwd.match(/.*[0-9]/)) return false;
+		//console.log(pwd, '0-9 OK');
+		if (!pwd.match(/.*[A-Z]/)) return false;
+		//console.log(pwd, 'A-Z OK');
+		if (!pwd.match(/.*[a-z]/)) return false;
+		//console.log(pwd, 'a-z OK');
+		return true;
 	}
 
 	for (var i = 0; i < 20; ++i)
