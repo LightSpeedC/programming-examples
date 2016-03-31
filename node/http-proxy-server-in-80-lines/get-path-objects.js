9c289d957b9816b38d848241347796edde14a413
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-03-31 23:47:27 +0900

    rename variables and revise

--- node/http-proxy-server-in-80-lines/get-path-objects.js
+++ node/http-proxy-server-in-80-lines/get-path-objects.js
@@ -1,42 +1,48 @@
-'use strict';
-var url = require('url');
-var o = {};
-var defport = {'http:':80, 'https:':443};
-function a(u){
-	var x = url.parse(u.indexOf('//') >= 0 ? u : u = 'http://' + u);
-	var n = getPathObjects(o, [x.protocol.split(':')[0]]
-		.concat(x.hostname.split('.').reverse())
-		.concat(x.port || defport[x.protocol])
-		.concat(x.path.split('/').filter(e => e)));
-	console.log(u);
-	//console.log(x);
-	console.log();
-}
-function getPathObjects(o, p) {
-	var n = o;
-	var a = [n];
-	p.forEach(e => {
-		if (!n.hasOwnProperty(e)) n[e] = {};
-		n = n[e];
-		if (!n._) n._ = {count:0, time:Date.now(), size:0};
-		n._.count++;
-		n._.time = Date.now();
-		n._.size += 100;
-		a.push(n);
-	});
-	//console.log(p);
-	return a;
-}
-module.exports = getPathObjects;
+void function () {
+	'use strict';
+	var url = require('url');
+	var root = {};
+	var ports = {'http:':80, 'https:':443};
+	var _stat = ' stat';
 
-if (module === require.main) {
-	a('http://www.google.com/index.html');
-	a('https://www.google.com/index.html');
-	a('http://www.facebook.com/index.html');
-	a('https://www.facebook.com/index.html');
-	a('http://www.group.net/index.html');
-	a('https://www.group.net/index.html');
-	a('www.facebook.com/index.html');
-	a('www.facebook.com:80');
-	console.log(require('util').inspect(o, {colors:true, depth:null}));
-}
+	function uri2arr(uri) {
+		var x = url.parse(uri.includes('//') ? uri : uri = 'http://' + uri);
+		return [x.protocol.split(':')[0]]
+			.concat(x.hostname.split('.').reverse())
+			.concat('port' + (x.port || ports[x.protocol]))
+			.concat(x.path.split('/').filter(elem => elem));
+	}
+
+	function getPathObjects(root, uri) {
+		var elems = uri2arr(uri);
+		var obj = root, arr = [obj];
+		elems.forEach(elem => {
+			if (!obj.hasOwnProperty(elem))
+				obj[elem] = {};
+
+			obj = obj[elem];
+			if (!obj[_stat])
+				obj[_stat] = {count:0, time:Date.now(), size:0};
+
+			obj[_stat].count++;
+			obj[_stat].time = Date.now();
+			obj[_stat].size += 100;
+			arr.push(obj);
+		});
+		return arr;
+	}
+	module.exports = getPathObjects;
+
+	// test code main
+	if (module === require.main) {
+		getPathObjects(root, 'http://www.google.com/index.html');
+		getPathObjects(root, 'https://www.google.com/index.html');
+		getPathObjects(root, 'http://www.facebook.com/index.html');
+		getPathObjects(root, 'https://www.facebook.com/index.html');
+		getPathObjects(root, 'http://www.group.net/index.html');
+		getPathObjects(root, 'https://www.group.net/index.html');
+		getPathObjects(root, 'www.facebook.com/index.html');
+		getPathObjects(root, 'www.facebook.com:80');
+		console.log(require('util').inspect(root, {colors:true, depth:null}));
+	}
+}();
