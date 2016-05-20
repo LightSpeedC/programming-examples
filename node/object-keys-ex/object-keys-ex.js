d97c36c9d066a1a68e597cd93419dde2a994e79a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-05-21 06:24:39 +0900

    space to tab

--- node/object-keys-ex/object-keys-ex.js
+++ node/object-keys-ex/object-keys-ex.js
@@ -1,36 +1,36 @@
-  var util = require('util');
+	var util = require('util');
 
-  console.log('########');
-  console.log('Object.getOwnPropertyNames');
-  fn1({}.__proto__);
-  function fn1(obj) {
-    Object.getOwnPropertyNames(obj).forEach(function (prop) {
-      console.log('\x1b[1;31m%s\x1b[m %s', prop, util.inspect(
-        Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
-          .replace(/\n /g,''));
-    });
-  }
+	console.log('########');
+	console.log('Object.getOwnPropertyNames');
+	fn1({}.__proto__);
+	function fn1(obj) {
+		Object.getOwnPropertyNames(obj).forEach(function (prop) {
+			console.log('\x1b[1;31m%s\x1b[m %s', prop, util.inspect(
+				Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
+					.replace(/\n /g,''));
+		});
+	}
 
-  console.log('########');
-  console.log('Object.keys');
-  fn2({}.__proto__);
-  function fn2(obj) {
-    Object.keys(obj).forEach(function (prop) {
-      console.log('\x1b[1;33m%s\x1b[m %s', prop, util.inspect(
-        Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
-          .replace(/\n /g,''));
-    });
-  }
+	console.log('########');
+	console.log('Object.keys');
+	fn2({}.__proto__);
+	function fn2(obj) {
+		Object.keys(obj).forEach(function (prop) {
+			console.log('\x1b[1;33m%s\x1b[m %s', prop, util.inspect(
+				Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
+					.replace(/\n /g,''));
+		});
+	}
 
-  console.log('########');
-  console.log('for in');
-  fn3({}.__proto__);
-  function fn3(obj) {
-    var keys = [];
-    for (var k in obj) keys.push(k);
-    keys.forEach(function (prop) {
-      console.log('\x1b[1;35m%s\x1b[m %s', prop, util.inspect(
-        Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
-          .replace(/\n /g,''));
-    });
-  }
+	console.log('########');
+	console.log('for in');
+	fn3({}.__proto__);
+	function fn3(obj) {
+		var keys = [];
+		for (var k in obj) keys.push(k);
+		keys.forEach(function (prop) {
+			console.log('\x1b[1;35m%s\x1b[m %s', prop, util.inspect(
+				Object.getOwnPropertyDescriptor(obj, prop), {colors:true})
+					.replace(/\n /g,''));
+		});
+	}
