af53ec269336859edc1afe34076e5461b572132c
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-04 17:03:09 +0900

    quick start main index.js

--- node-webkit/quick-start-main/index.js
+++ node-webkit/quick-start-main/index.js
@@ -1,17 +1,30 @@
-try {
-	if (!global.count) global.count = 0;
-	//if (global === window) alert('global === window');
+void function () {
+	'use strict';
 
-	const cfg = require('./package.json');
-	global.hey = function () {
-		nw.Window.open('index.html', cfg.window, function (win) {
-			setTimeout(() => win.close(), 7e3);
-		});
-		return false;
+	try {
+		// カウント
+		if (!global.myCount) global.myCount = 0;
+
+		// package.jsonのwindowオプションの初期値を取り込む
+		const options = require('./package.json').window || {};
+		const x = options.x = options.x || 200;
+		const y = options.y = options.y || 100;
+
+		// Windowを開く
+		global.openMyWindow = function openMyWindow() {
+			nw.Window.open('index.html', options, function (win) {
+				++myCount;
+			});
+			options.x += 40;
+			options.y += 10;
+			if (options.x >= 600 + myCount * 5 ||
+				options.y >= 200 - myCount) {
+				options.x = x + myCount * 5;
+				options.y = y - myCount;
+			}
+		}
+		openMyWindow();
+	} catch (e) {
+		alert(e.stack);
 	}
-
-	hey();
-
-} catch (e) {
-	alert(e.stack);
-}
+}();
