e906404db2da8322eb8f5703ce8baa25c1f88261
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-06 07:08:54 +0900

    debug print

--- node-webkit/quick-start-main/main.js
+++ node-webkit/quick-start-main/main.js
@@ -3,3 +3,12 @@ const options = require('./package.json').window || {};
 
 // Windowを開く
 nw.Window.open('index.html', options);
+
+require('fs').writeFileSync('process.log',
+	['process.pid = ' + process.pid,
+	'process.argv = ' + process.argv.join(' '),
+	'App.manifest.name = ' + nw.App.manifest.name,
+	'App.argv = ' + nw.App.argv.join(' '),
+	'App.fullArgv = ' + nw.App.fullArgv.join(' '),
+	'App.filteredArgv = ' + nw.App.filteredArgv.join(' '),
+	''].join('\n'));
