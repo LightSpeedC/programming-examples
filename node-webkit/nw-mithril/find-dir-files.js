6dc80626c9f2c262250138d5afa2377cc0fe68c8
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-08 07:28:17 +0900

    find dir files & async

---
+++ node-webkit/nw-mithril/find-dir-files.js
@@ -0,0 +1,44 @@
+// ディレクトリやファイルを探す
+
+void function () {
+	'use strict';
+
+	module.exports = findDirFiles;
+
+	const path = require('path');
+	const fs = require('fs');
+	const aa = require('aa');
+	const statAsync = aa.thunkify(fs, fs.stat);
+	const readdirAsync = aa.thunkify(fs, fs.readdir);
+	if (typeof statAsync !== 'function')
+		throw new TypeError('eh!? statAsync');
+	if (typeof readdirAsync !== 'function')
+		throw new TypeError('eh!? readdirAsync');
+	//aa.thunkifyAll(fs);
+	//if (typeof fs.statAsync !== 'function')
+	//	throw new TypeError('eh!? statAsync');
+	//if (typeof fs.readdirAsync !== 'function')
+	//	throw new TypeError('eh!? readdirAsync');
+
+	function *findDirFiles(dir, pattern, progressCallback, wholeObject) {
+		const names = yield readdirAsync(dir);
+		const result = {};
+		if (!wholeObject) wholeObject = result;
+		yield names.map(name => function *() {
+			const subDir = path.resolve(dir, name);
+			const stat = yield statAsync(subDir);
+			if (stat.isDirectory()) {
+				const r = yield findDirFiles(subDir, pattern, progressCallback, wholeObject);
+				if (r || name.includes(pattern))
+					result[name + path.sep] = r ||
+						(progressCallback(subDir + path.sep, wholeObject), null) || {};
+			}
+			else if (name.includes(pattern)) {
+				result[name] = null;
+				progressCallback(subDir, wholeObject);
+			}
+		});
+		return (Object.keys(result).length || null) && result;
+	}
+
+}();
