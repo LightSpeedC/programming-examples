fe7dcce35576ec4463afd02aa330fdfac8199f75
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-07 20:21:50 +0900

    support nwjs/node-webkit, electron and chrome browser

--- node-webkit/nw-mithril/main.js
+++ node-webkit/nw-mithril/main.js
@@ -1,5 +1,32 @@
-// package.jsonのwindowオプションの初期値を取り込む
-const options = require('./package.json').window || {};
+void function () {
+	'use strict';
 
-// Windowを開く
-nw.Window.open('index.html', options);
+	// package.jsonのwindowオプションの初期値を取り込む
+	const options = require('./package.json').window;
+
+	// NWjs/node-webkit
+	if (global.nw) {
+		// Windowを開く
+		nw.Window.open('index.html', options);
+		return;
+	}
+
+	// Electron/Atom-Shell
+	const electron = require('electron');
+	const {app, BrowserWindow} = electron;
+
+	const wins = new Map();
+
+	function createWindow() {
+		const win = new BrowserWindow(options);
+		wins.set(win, true);
+		win.loadURL('file://' + __dirname + '/index.html');
+		win.webContents.openDevTools();
+		win.on('closed', () => wins.delete(win));
+	}
+
+	app.on('ready', createWindow);
+	app.on('window-all-closed',
+		() => process.platform !== 'darwin' && app.quit());
+	app.on('activate', () => wins.size || createWindow());
+}();
