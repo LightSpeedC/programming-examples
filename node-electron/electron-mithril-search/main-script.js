d125a6778eafbbfc22a17c4d40aeba07d9b03f67
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-13 07:19:05 +0900

    add release notes

--- node-electron/electron-mithril-search/main-script.js
+++ node-electron/electron-mithril-search/main-script.js
@@ -3,7 +3,7 @@ void function () {
 
 	focus();
 
-	const version = 'version: 0.0.5 (2016/06/12)';
+	const version = 'version: 0.0.5 (2016/06/13)';
 	const path = require('path');
 	const spawn = require('child_process').spawn;
 	const aa = require('aa');
@@ -11,6 +11,7 @@ void function () {
 	const findController = {isCancel:false, cancel, progress};
 
 	const debugFlag = m.prop(false);
+	const releaseNotesFlag = m.prop(false);
 	const includes = m.prop('');
 	const excludes = m.prop('');
 	const message = m.prop('検索できます');
@@ -18,12 +19,13 @@ void function () {
 	const targetDir = process.env.AAA_TARGET_DIR;
 	const text = m.prop('');
 	const newLayout = m.prop(true); // 新レイアウト
-	let files = [];
+	let files = [targetDir + '\まだ検索していません'];
 	let filesIsDirty = false;
-	let wholeObject = {};
+	let wholeObject = {[findDirFiles.ERROR_PROP]: 'まだ検索していません'};
 	let timer;
 	const maxIndent = 20;
 	const indentSpace = ' ';
+	const HIDE_PROP = ' hide';
 
 	m.mount($div, {view});
 
@@ -68,16 +70,26 @@ void function () {
 			m('hr', {key: 'hr3'}),
 			(newLayout() ? myViewResult2() : myViewResult())), // 検索結果
 			m('hr', {key: 'hr4'}),
-			m('div', {key:'version'}, version), // バージョン
+			// デバッグ表示
+			m('div', {key: 'release-notes'},
+				m('input[type=checkbox]',
+					m_on('click', 'checked', releaseNotesFlag)),
+				'リリースノート: ' + version, (releaseNotesFlag() ? m('pre', {}, m('font[color=purple]', [
+					'\t0.0.5 (2016/06/13): リリースノート表示、リファクタリング\n',
+					'\t0.0.4 (2016/06/12): ツリー表示、新旧レイアウト切替\n',
+					'\t0.0.3 (2016/06/11): 検索中のキャンセル\n',
+					'\t0.0.2 (2016/06/10): フォルダやファイルへのリンク\n',
+					'\t0.0.1 (2016/06/09): 検索したファイルの一覧表示、フィルタ(含む＋含まない)\n',
+				])) : '')),
 			// デバッグ表示
 			m('div', {key: 'debug'},
 				m('input[type=checkbox]',
 					m_on('click', 'checked', debugFlag)),
-				'debug', (debugFlag() ? debugView() : ''))
+				'デバッグ表示 (環境変数など)', (debugFlag() ? debugView() : ''))
 		];
 	}
 
-	// 結果
+	// 結果表示 単純リスト形式
 	function myViewResult() {
 		if (filesIsDirty) files = files.sort();
 		filesIsDirty = false;
@@ -114,11 +126,12 @@ void function () {
 		return a;
 	}
 
+	// ツリー表示
 	function viewNode(nodePath, node, i, n, txt, incl, excl) {
 		if (typeof node === 'object' && node !== null) {
 			let keys = Object.getOwnPropertyNames(node);
 			return [
-				keys.filter(prop => prop !== ' hide').map(prop => {
+				keys.filter(prop => prop !== HIDE_PROP).map(prop => {
 					const child = node[prop];
 					if (prop === findDirFiles.ERROR_PROP)
 						return m('tr', {},
@@ -135,8 +148,8 @@ void function () {
 									child ? m('input[type=checkbox]',
 										m_on('click', 'checked',
 											function (v) {
-												if (arguments.length === 0) return child[' hide'] !== true;
-												child[' hide'] = !v;
+												if (arguments.length === 0) return child[HIDE_PROP] !== true;
+												child[HIDE_PROP] = !v;
 											}
 										)
 									) : indentSpace
@@ -148,7 +161,7 @@ void function () {
 								}, prop)
 							] : []
 						),
-						(!child || child[' hide']) ? [] :
+						(!child || child[HIDE_PROP]) ? [] :
 						viewNode(nodePath.concat(prop), child, i + 1, n, txt, incl, excl)
 					];
 				})
@@ -158,7 +171,7 @@ void function () {
 			return [];
 	}
 
-	// 結果2
+	// 結果表示2 ツリー形式
 	function myViewResult2() {
 		const txt = text();
 		const incl = includes();
@@ -229,10 +242,10 @@ void function () {
 	function debugView() {
 		return m('pre',
 			m('font[color=brown]',
-				('process.cwd(): ' + process.cwd()),
-				('\n\nprocess.pid: ' + process.pid),
-				('\n\nprocess.versions: ' + JSON.stringify(process.versions, null, '\t')),
-				('\n\nprocess.env: ' + JSON.stringify(process.env, null, '\t'))
+				('カレント作業ディレクトリ process.cwd(): ' + process.cwd()),
+				('\n\nプロセスID process.pid: ' + process.pid),
+				('\n\nバージョン process.versions: ' + JSON.stringify(process.versions, null, '\t')),
+				('\n\n環境変数 process.env: ' + JSON.stringify(process.env, null, '\t'))
 			));
 	}
 
