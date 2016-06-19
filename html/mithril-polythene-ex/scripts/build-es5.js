2348a3546ad1bca360fa7ed4d7f9457f1a904926
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-19 14:41:59 +0900

    space to tab

--- html/mithril-polythene-ex/scripts/build-es5.js
+++ html/mithril-polythene-ex/scripts/build-es5.js
@@ -2,21 +2,21 @@ var fs = require('fs');
 var browserify = require('browserify');
 
 function bundle(entries, outfile) {
-    browserify({
-        entries: entries,
-        extensions: ['.es5.js'],
-        paths: ['.']
-    })
-    .transform({
-        global: true
-    }, 'uglifyify')
-    .bundle()
-    .on('error', function(err) {
-        console.log('Error : ' + err.message);
-    })
-    .pipe(fs.createWriteStream(outfile));
+	browserify({
+		entries: entries,
+		extensions: ['.es5.js'],
+		paths: ['.']
+	})
+	.transform({
+		global: true
+	}, 'uglifyify')
+	.bundle()
+	.on('error', function(err) {
+		console.log('Error : ' + err.message);
+	})
+	.pipe(fs.createWriteStream(outfile));
 };
 
 bundle([
-    'app/index/index.es5.js'
+	'app/index/index.es5.js'
 ], 'app-es5.js');
