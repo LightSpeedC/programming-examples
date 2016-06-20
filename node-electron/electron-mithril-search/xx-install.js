62d1af901f8aa3c5455fa177347af715197db72a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-21 06:34:57 +0900

    revise installer

--- node-electron/electron-mithril-search/xx-install.js
+++ node-electron/electron-mithril-search/xx-install.js
@@ -1,9 +1,25 @@
 void function () {
 	'use strict';
 
+	const dirRex = /\$\$\$/g;
+	const exeRex = /###/g;
+	const yenRex = /\\/g;
+	const yen2Str = '\\\\';
+
+	const child_process = require('child_process');
 	const fs = require('fs');
-	var b = fs.readFileSync('xx-e.reg');
-	var c = b.toString('UTF16LE');
-	var d = new Buffer(c, 'UTF16LE');
-	fs.writeFileSync('xxx-e.reg', d);
+
+	let res = child_process.spawnSync('where', ['electron.exe']);
+	if (res.status || res.signal)
+		return console.error(res.status, res.signal);
+	const eleExe = res.stdout.toString().split('\n')[0].trim();
+
+	let b = fs.readFileSync('xx-e.reg').toString('UTF16LE');
+	b = b.replace(dirRex, __dirname.replace(yenRex, yen2Str))
+	.replace(exeRex, eleExe.replace(yenRex, yen2Str));
+	fs.writeFileSync('xx-e.reg.log', new Buffer(b, 'UTF16LE'));
+
+	res = child_process.spawnSync('regedit', ['/s', 'xx-e.reg.log']);
+	if (res.status || res.signal)
+		return console.error(res.status, res.signal);
 } ();
