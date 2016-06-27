edbb1013547a680b1fc03497b52d24aca512b978
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-28 00:12:33 +0900

    revise

--- node/spawn2-ex/main.js
+++ node/spawn2-ex/main.js
@@ -2,13 +2,13 @@ void function () {
 	'use strict';
 
 	if (require.main === module) {
-		var main = require('./' + (process.argv[2] || 'main'));
-		var spawn = require('./' + (process.argv[3] || 'main'));
+		var main = require('./' + (process.argv[2] || 'main1'));
+		var spawn = require('./' + (process.argv[3] || 'aa2'));
 		var r = spawn(main);
 		if (r && r.then) r.then(
-			function (v) { console.log('v', v); },
-			function (e) { console.log('e', e + ''); });
-		else if (typeof r === 'function') r(function (e, v) { console.log('ev', e + '', v); });
+			function (v) { console.log('v:', v); },
+			function (e) { console.log('e:', e + ''); });
+		else if (typeof r === 'function') r(function (e, v) { console.log('e:', e + ',', 'v:', v); });
 	}
 
 } ();
