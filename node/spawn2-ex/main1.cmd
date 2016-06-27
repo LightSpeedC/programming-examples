3f9de1ce7def37f822317e03ddec7e6947a1a894
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-28 00:09:38 +0900

    revise

---
+++ node/spawn2-ex/main1.cmd
@@ -0,0 +1,11 @@
+node main main1 spawn31
+node main main1 spawn32
+node main main1 spawn3
+node main main1 spawn4
+node main main1 spawn5
+node main main1 aa1
+node main main1 aa2
+node main main1 co3
+node main main1 co4
+@if "%paused%" == "" pause
+@set paused=paused
