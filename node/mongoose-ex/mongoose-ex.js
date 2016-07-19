892545793c54ae890c3d430355f8245223c88298
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-07-19 19:56:18 +0900

    mongoose example

---
+++ node/mongoose-ex/mongoose-ex.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var Cat = mongoose.model('Cat', {name: String});
+var kitty = new Cat({name: 'Zildjian'});
+kitty.save(function (err) {
+	console.log(err || 'meow');
+});
