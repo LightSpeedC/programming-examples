6640721968a84d86ecc69d23b7b051b054dd9645
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-15 22:22:13 +0900

    object map reduce filter forEach etc

---
+++ node/object-map-reduce-ex/object-map-reduce.js
@@ -0,0 +1,55 @@
+
+	void function (obj) {
+		for (var prop in obj)
+			Object.prototype[prop] ||
+			Object.defineProperty(Object.prototype, prop, {
+				configureable: true,
+				value: obj[prop]});
+	} ({
+		forEach: function forEach(fn) {
+			for (var prop in this)
+				if (this.hasOwnProperty(prop))
+					fn(this[prop], prop, this);
+		},
+		map: function map(fn) {
+			var obj = {};
+			for (var prop in this)
+				if (this.hasOwnProperty(prop))
+					obj[prop] = fn(this[prop], prop, this);
+			return obj;
+		},
+		filter: function filter(fn) {
+			var obj = {};
+			for (var prop in this)
+				if (this.hasOwnProperty(prop))
+					if (fn(this[prop], prop, this))
+						obj[prop] = this[prop];
+			return obj;
+		},
+		reduce: function reduce(fn, init) {
+			var first = true;
+			for (var prop in this)
+				if (this.hasOwnProperty(prop))
+					first && arguments.length <= 1 ?
+					init = this[prop]:
+					init = fn(init, this[prop], prop, this),
+					first = false;
+			return init;
+		},
+		reduceRight: function reduceRight(fn, init) {
+			var first = true, self = this, len = arguments.length;
+			Object.keys(this).reverse().forEach(function (prop) {
+				if (self.hasOwnProperty(prop))
+					first && len <= 1 ?
+					init = self[prop]:
+					init = fn(init, self[prop], prop, self),
+					first = false;
+			});
+			return init;
+		}
+	});
+
+	console.log({x:1,y:2,z:3}.reduce((x,y) => x + y));
+	console.log({x:1,y:2,z:3}.reduce((x,y) => x + y, 1));
+	console.log({x:1,y:2,z:3}.reduceRight((x,y) => x + y));
+	console.log({x:1,y:2,z:3}.reduceRight((x,y) => x + y, 1));
