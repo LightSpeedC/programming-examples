76d813d1a0bcf9657976b1730e4a9cb7317ec48e
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-19 09:48:29 +0900

    move static files . to public

--- node/express-ex/ex10-express-static-simple/app.js
+++ node/express-ex/ex10-express-static-simple/app.js
@@ -1,1 +1,8 @@
-require('express')().use(require('express').static('.')).listen(process.argv[2] || process.env.PORT || 3000);
+void function () {
+	'use strict';
+	const express = require('express'), app = express();
+
+	app.use(express.static('public'));
+
+	app.listen(process.argv[2] || process.env.PORT || 3000);
+} ();
