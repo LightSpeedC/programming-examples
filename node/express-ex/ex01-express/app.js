da3388ceebac1dcacd3cc5090f16b5d0bdf87533
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-19 09:31:53 +0900

    express 4.14.0, change var to let/const

--- node/express-ex/ex01-express/app.js
+++ node/express-ex/ex01-express/app.js
@@ -1,31 +1,31 @@
-var express = require('express');
-var app = express();
-var debug = require('debug')('ex01-express:server');
+const express = require('express');
+const app = express();
+const debug = require('debug')('ex01-express:server');
 
 // respond with "Hello World!" on the homepage
 // respond with "Hello World!" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 // accept POST request on the homepage
 app.post('/', function (req, res) {
-  res.send('Got a POST request');
+	res.send('Got a POST request');
 });
 
 // accept PUT request at /user
 app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user');
+	res.send('Got a PUT request at /user');
 });
 
 // accept DELETE request at /user
 app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
+	res.send('Got a DELETE request at /user');
 });
 
 app.all('/secret', function (req, res, next) {
-  debug('Accessing the secret section ...');
-  next();  // pass control to the next handler
+	debug('Accessing the secret section ...');
+	next();	// pass control to the next handler
 });
 
 // public files
@@ -34,21 +34,21 @@ app.use(express.static('public'));
 
 // handle error
 app.use(function(req, res, next){
-  res.status(404).send('Sorry cant find that!');
-  //res.send(404, 'Sorry cant find that!');
+	res.status(404).send('Sorry cant find that!');
+	//res.send(404, 'Sorry cant find that!');
 });
 
 // handle error
 //app.use(function(err, req, res, next){
-//  console.error(err.stack);
-//  res.send(500, 'Something broke!');
+//	console.error(err.stack);
+//	res.send(500, 'Something broke!');
 //});
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(3000, function () {
+	let host = server.address().address;
+	const port = server.address().port;
 
-  if (host === '::') host = 'localhost';
+	if (host === '::') host = 'localhost';
 
-  debug('Example app listening at http://%s:%s', host, port);
+	debug('Example app listening at http://%s:%s', host, port);
 });
