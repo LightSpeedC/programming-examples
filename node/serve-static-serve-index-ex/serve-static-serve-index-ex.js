e7051be623a7dfbd74293fef12995f8ce4ec4dee
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-25 10:13:23 +0900

    serve-static-serve-index-ex

---
+++ node/serve-static-serve-index-ex/serve-static-serve-index-ex.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Serve directory indexes for current folder (with icons)
+const serveIndex = require('serve-index')('.', {icons: true});
+// Serve up current folder files
+const serveStatic = require('serve-static')('.');
+const finalhandler = require('finalhandler');
+
+// Create http server 
+require('http').createServer(function onRequest(req, res) {
+	console.log(req.method, req.url);
+	const done = finalhandler(req, res);
+	//const done = function (err) {
+	//	res.statusCode = 404;
+	//	res.end(err ? err + '' : '404 Not Found : ' + req.url);
+	//};
+	serveStatic(req, res, function onNext(err) {
+		if (err) return done(err)
+		serveIndex(req, res, done)
+	})
+})
+// Listen
+.listen(3000);
