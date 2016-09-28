79b702c510b49e883409295905257d92a2ae5db8
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-29 06:54:27 +0900

    change 2 spaces to tab

--- html/xhr-ex/here.js
+++ html/xhr-ex/here.js
@@ -1,33 +1,33 @@
-  var mimes = {'.html': 'text/html', '.js': 'text/javascript'};
-  var fs = require('fs');
-  var util = require('util');
-  var path = require('path');
-  var http = require('http')
-  var dirnameRex = RegExp((__dirname + '\\').replace(/\\/g, '(/|\\\\\\\\?)'), 'g');
-  http.createServer(function (req, res) {
-    var file = req.url;
-    if (file === '/echo') {
-      console.log('--:', req.url);
-      res.writeHead(200, {'content-type': 'application/json'});
-      req.pipe(res);
-      return;
-    }
+	var mimes = {'.html': 'text/html', '.js': 'text/javascript'};
+	var fs = require('fs');
+	var util = require('util');
+	var path = require('path');
+	var http = require('http')
+	var dirnameRex = RegExp((__dirname + '\\').replace(/\\/g, '(/|\\\\\\\\?)'), 'g');
+	http.createServer(function (req, res) {
+		var file = req.url;
+		if (file === '/echo') {
+			console.log('--:', req.url);
+			res.writeHead(200, {'content-type': 'application/json'});
+			req.pipe(res);
+			return;
+		}
 
-    if (file.endsWith('/')) file += 'index.html';
-    file = path.join(__dirname, file);
-    console.log('--:', req.url, file);
-    fs.readFile(file, function (err, data) {
-      if (err) {
-        console.log('NG:', req.url, file, err);
-        res.writeHead(404, {'content-type': 'text/plain'});
-        res.end(util.inspect(err).replace(dirnameRex, '?/'));
-      }
-      else {
-        console.log('OK:', req.url, file);
-        res.writeHead(200, {'content-type': mimes[path.extname(file)]});
-        res.end(data);
-      }
-    });
-  }).listen(8080, function () {
-    console.log('listen start...');
-  });
+		if (file.endsWith('/')) file += 'index.html';
+		file = path.join(__dirname, file);
+		console.log('--:', req.url, file);
+		fs.readFile(file, function (err, data) {
+			if (err) {
+				console.log('NG:', req.url, file, err);
+				res.writeHead(404, {'content-type': 'text/plain'});
+				res.end(util.inspect(err).replace(dirnameRex, '?/'));
+			}
+			else {
+				console.log('OK:', req.url, file);
+				res.writeHead(200, {'content-type': mimes[path.extname(file)]});
+				res.end(data);
+			}
+		});
+	}).listen(8080, function () {
+		console.log('listen start...');
+	});
--- html/xhr-ex/mini-xhr-ex.js
+++ html/xhr-ex/mini-xhr-ex.js
@@ -1,15 +1,15 @@
-  var MiniXHR = require('./mini-xhr');
+	var MiniXHR = require('./mini-xhr');
 
-  var xhr = new MiniXHR('http://localhost:8080/');
-  var xhr2 = new MiniXHR('http://localhost:8080/');
+	var xhr = new MiniXHR('http://localhost:8080/');
+	var xhr2 = new MiniXHR('http://localhost:8080/');
 
-  xhr.get('index.html')
-  .then(
-    function (val) { console.log('get index.html', val.length); },
-    function (err) { console.log('get index.html', err); });
-  xhr2.post('echo', {x:1,y:2})
-  .then(
-    function (val) { console.log('post echo', val); },
-    function (err) { console.log('post echo', err); });
+	xhr.get('index.html')
+	.then(
+		function (val) { console.log('get index.html', val.length); },
+		function (err) { console.log('get index.html', err); });
+	xhr2.post('echo', {x:1,y:2})
+	.then(
+		function (val) { console.log('post echo', val); },
+		function (err) { console.log('post echo', err); });
 
-  // /xhr.request();
+	// /xhr.request();
