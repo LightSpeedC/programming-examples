6012e78c9841d8d1c26621bcafd6c1f11675cf06
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-11 10:24:49 +0900

    copy min js files

--- html/react-gulp-ex/ex4/gulpfile.js
+++ html/react-gulp-ex/ex4/gulpfile.js
@@ -1,12 +1,15 @@
+'use strict';
+
 const gulp = require('gulp');
+const task = gulp.task.bind(gulp);
 //const plumber = require('gulp-plumber');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const literalify = require('literalify');
 const fs = require('fs');
 
-gulp.task('browserify', function () {
-	return browserify({debug: true})
+task('browserify', () =>
+	browserify({debug: true})
 		.transform(babelify.configure({
 			presets: ['es2015', 'react']
 		}))
@@ -17,14 +20,18 @@ gulp.task('browserify', function () {
 		}))
 		.require('./src/app.jsx', {entry: true})
 		.bundle()
-		.on('error', function (err) { console.log('Error:', err.message); })
-		.pipe(fs.createWriteStream('bundle.js'));
-});
+		.on('error', err => console.log('Error:', err.message))
+		.pipe(fs.createWriteStream('bundle.js')));
 
-gulp.task('watch', function() {
-	gulp.watch('./src/*.jsx', ['browserify'])
-});
+task('watch', () =>
+	gulp.watch('./src/*.jsx', ['browserify']));
 
-gulp.task('default', ['watch', 'browserify']);
+task('copy-min-js', () =>
+	gulp.src(['./node_modules/react/dist/*.min.js',
+			'./node_modules/react-dom/dist/*.min.js',
+			'./node_modules/react-router/umd/*.min.js'])
+		.pipe(gulp.dest('./js')));
+
+task('default', ['copy-min-js', 'watch', 'browserify']);
 
 // http://qiita.com/momunchu/items/dcd95d186ac3b74d1edf
