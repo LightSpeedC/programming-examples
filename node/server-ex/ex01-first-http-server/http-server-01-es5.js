dc7df88b391680ecd946eb0f6e8bc86587fcb7cf
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-24 23:11:37 +0900

    remove hostname

--- node/server-ex/ex01-first-http-server/http-server-01-es5.js
+++ node/server-ex/ex01-first-http-server/http-server-01-es5.js
@@ -1,6 +1,5 @@
 var http = require('http');
 
-var hostname = '127.0.0.1';
 var port = 3000;
 
 var server = http.createServer(function (req, res) {
@@ -9,6 +8,6 @@ var server = http.createServer(function (req, res) {
 	res.end('Hello World\n');
 });
 
-server.listen(port, hostname, function () {
-	console.log('Server running at http://' + hostname + ':' + port + '/');
+server.listen(port, function () {
+	console.log('Server running at http://localhost:' + port + '/');
 });
