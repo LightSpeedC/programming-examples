0b44df83f299b9d326bbe7e718d838b431370e2e
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-01-11 00:11:00 +0900

    module.parent, require.main

--- node/module-parent-require-main/main.js
+++ node/module-parent-require-main/main.js
@@ -1,2 +1,4 @@
 console.log('main:', require.main === module, module.parent == null);
+console.log(module);
+console.log();
 require('./sub');
--- node/module-parent-require-main/sub.js
+++ node/module-parent-require-main/sub.js
@@ -1,1 +1,3 @@
 console.log('sub:', require.main === module, module.parent == null);
+console.log(module);
+console.log();
