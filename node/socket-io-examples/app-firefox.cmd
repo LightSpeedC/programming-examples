bc2a8d2a0a5b5563ebd326e84bf37320740c2c12
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-02-03 06:45:06 +0900

    title

--- node/socket-io-examples/app-firefox.cmd
+++ node/socket-io-examples/app-firefox.cmd
@@ -1,10 +1,10 @@
 @set ff1=C:\Program Files\Mozilla Firefox\firefox.exe
 @set ff2=C:\Program Files (x86)\Mozilla Firefox\firefox.exe
 @if exist "%ff1%" (
-	start "GP-WAN" "%ff1%" http://localhost:3000
+	start "title" "%ff1%" http://localhost:3000
 	goto ok)
 @if exist "%ff2%" (
-	start "GP-WAN" "%ff2%" http://localhost:3000
+	start "title" "%ff2%" http://localhost:3000
 	goto ok)
 @start http://localhost:3000
 @:ok
