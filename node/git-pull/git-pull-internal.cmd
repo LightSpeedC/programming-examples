c4f38a95447984ff4f0679cd39662115b18f699b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-02-02 16:53:30 +0900

    git pull internal (no proxy)

---
+++ node/git-pull/git-pull-internal.cmd
@@ -0,0 +1,9 @@
+@set HTTP_PROXY=
+@set HTTPS_PROXY=
+@if not exist node_modules call npm install
+@pushd %~dp0
+@set GIT_ROOT_DIR=%1
+@if "%GIT_ROOT_DIR%" == "" set GIT_ROOT_DIR=..\..\..\..
+node git-pull %GIT_ROOT_DIR%
+@popd
+@pause
