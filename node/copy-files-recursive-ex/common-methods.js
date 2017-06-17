8766cc733c78a751917489ede1a599b0ebb6715a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-06-17 11:13:29 +0900

    [add] all method for array and object

--- node/copy-files-recursive-ex/common-methods.js
+++ node/copy-files-recursive-ex/common-methods.js
@@ -4,6 +4,7 @@ function commonMethods(Base) {
 	Base.resolve || (Base.resolve = resolve);
 	Base.reject || (Base.reject = reject);
 	Base.wait || (Base.wait = wait);
+	Base.all || (Base.all = all);
 
 	function resolve(val) {
 		return Base(function (res, rej) {
@@ -22,6 +23,47 @@ function commonMethods(Base) {
 			setTimeout(res, msec, val);
 		});
 	} // wait
+
+	function all(arg, cb) {
+		if (arg == null ||
+			(arg.constructor !== Array && arg.constructor !== Object))
+			throw new TypeError('first argument is not an array or an object');
+
+		return arg.constructor === Array ? allArray(arg, cb) : allObject(arg, cb);
+	} // all
+
+	function allArray(arr, cb) {
+		//if (arr == null || arr.constructor !== Array)
+		//	throw new TypeError('first argument is not an array');
+
+		return Base(function (res, rej) {
+			var n = arr.length;
+			if (n === 0) { res([]); return; }
+
+			var result = new Array(n);
+			arr.forEach((p, i) => p.then(val => {
+				result[i] = val;
+				--n || res(result);
+			}, rej));
+		}, cb);
+	} // allArray
+
+	function allObject(obj, cb) {
+		//if (obj == null || obj.constructor !== Object)
+		//	throw new TypeError('first argument is not an object');
+
+		return Base(function (res, rej) {
+			var keys = Object.keys(obj);
+			var n = keys.length;
+			if (n === 0) { res({}); return; }
+
+			var result = keys.reduce((that, prop) => (that[prop] = undefined, that), {});
+			keys.forEach(prop => obj[prop].then(val => {
+				result[prop] = val;
+				--n || res(result);
+			}, rej));
+		}, cb);
+	} // allObject
 
 	return Base;
 };
