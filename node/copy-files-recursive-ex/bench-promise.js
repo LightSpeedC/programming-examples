c0d18042e0a9cf0b33aaf129547b724e4727699f
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-06-17 14:33:25 +0900

    support Dummy promise with callback mode

--- node/copy-files-recursive-ex/bench-promise.js
+++ node/copy-files-recursive-ex/bench-promise.js
@@ -4,7 +4,10 @@ function benchPromise() {
 	(typeof commonMethods === 'function' ? commonMethods : require('./common-methods'))(Dummy);
 	(typeof commonAa === 'function' ? commonAa : require('./common-aa'))(Dummy);
 
-	function Dummy(setup) {
+	function Dummy(setup, cb) {
+		if (typeof cb === 'function')
+			return new Promise(setup).then(val => cb(null, val), cb);
+
 		return new Promise(setup);
 	}
 
