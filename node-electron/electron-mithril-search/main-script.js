bf043796d163a0e10ff6a1bf8298f042cade9a02
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-07-16 09:53:54 +0900

    support mac

--- node-electron/electron-mithril-search/main-script.js
+++ node-electron/electron-mithril-search/main-script.js
@@ -3,7 +3,7 @@ void function () {
 
 	focus();
 
-	const version = 'version: 0.0.14 (2016/10/25)';
+	const version = 'version: 0.0.15 (2017/07/15)';
 
 	const path = require('path');
 	const spawn = require('child_process').spawn;
@@ -12,10 +12,10 @@ void function () {
 
 	const Rexfer = require('./rexfer');
 	const Config = require('./config-local-storage');
-	const config = new Config('node-electron-search', {});
+	const config = new Config('electron-mithril-async-file-search', {});
 
 	const findDirFiles = require('./find-dir-files');
-	const findController = {isCancel:false, cancel, progress};
+	const findController = { isCancel: false, cancel, progress };
 	const INVISIBLE_PROP = findDirFiles.INVISIBLE_PROP;
 	const CLEAN_PROP = findDirFiles.CLEAN_PROP;
 	const ERROR_PROP = findDirFiles.ERROR_PROP;
@@ -47,13 +47,13 @@ void function () {
 	const textMessage = m.prop('検索できます');
 
 	const targetDir = process.env.AAA_TARGET_DIR;
-	let wholeObject = {[ERROR_PROP]: 'まだ検索していません'};
+	let wholeObject = { [ERROR_PROP]: 'まだ検索していません' };
 	let timer; // 検索中のインターバルタイマー
 	const MAX_INDENT = 20; // 最大インデントの深さ
 	const indentSpace = '　';
-	const SUBTREE_RETAIN = {subtree: 'retain'};
+	const SUBTREE_RETAIN = { subtree: 'retain' };
 
-	m.mount($div, {view: viewMain});
+	m.mount($div, { view: viewMain });
 
 	// 使い方表示
 	function viewUsage() {
@@ -80,6 +80,7 @@ void function () {
 	// リリース・ノート表示
 	function viewReleaseNotes() {
 		const list = [
+			'0.0.15 (2017/07/15): WindowsだけでなくMacにも対応 for Gunma.web #28 (electron@1.5.1)',
 			'0.0.14 (2016/10/25): デフォルト値を変更 configバージョン変更',
 			'0.0.13 (2016/10/10): 起動時に入力テキストを選択',
 			'0.0.12 (2016/09/29): チェックボックスのラベルの不具合修正',
@@ -123,13 +124,13 @@ void function () {
 	// メインview
 	function viewMain() {
 		return [
-			m('h3', {key: 'title'}, 'ファイル検索 - ',
+			m('h3', { key: 'title' }, 'ファイル検索 - ',
 				m('font[color=green]', targetDir),
 				(timer ? m('font[color=red]', ' - 検索中') : '')),
-			m('div', {key: 'condition'},
+			m('div', { key: 'condition' },
 				timer ? [
 					m('div',
-						m('button.button', {onclick: cancel}, 'ｷｬﾝｾﾙ'),
+						m('button.button', { onclick: cancel }, 'ｷｬﾝｾﾙ'),
 						m('span', ' '),
 						m('b', m('font[color=red]', textSearch()))),
 					textIncludes() || textExcludes() ? [m('hr'), m('div', '検索結果フィルタ')] : '',
@@ -142,116 +143,120 @@ void function () {
 					m('div', '検索したくないファイルやフォルダ: 「', textSearchSkip(), '」'),
 					m('div', '検索時に閉じておくフォルダ: 「', textClose(), '」'),
 				] : [
-					m('font[color=gray][size=1]', '※ワイルドカード*?検索できます。' +
-						'空白でAND、コンマとセミコロンでOR、マイナスでNOT。' +
-						'優先順位：マイナス、コンマ、空白、セミコロンの順。'),
+						m('font[color=gray][size=1]', '※ワイルドカード*?検索できます。' +
+							'空白でAND、コンマとセミコロンでOR、マイナスでNOT。' +
+							'優先順位：マイナス、コンマ、空白、セミコロンの順。'),
 
-					// 検索
-					m('form', {onsubmit: search},
-						m('div', '検索: ',
-							m('input', m_on('change', 'value', textSearch,
-								{autofocus: true, placeholder: '検索したい文字列', size: 100,
-								 onfocus: selectThisText})),
-							m('button.button[type=submit]', {onclick: search}, '検索'))),
-					//m('hr'),
+						// 検索
+						m('form', { onsubmit: search },
+							m('div', '検索: ',
+								m('input', m_on('change', 'value', textSearch,
+									{
+										autofocus: true, placeholder: '検索したい文字列', size: 100,
+										onfocus: selectThisText
+									})),
+								m('button.button[type=submit]', { onclick: search }, '検索'))),
+						//m('hr'),
 
-					// 検索オプション
-					m('div',
-						m('input[type=checkbox]',
-							m_on('click', 'checked', flagAdvanced)),
-						m('font[color=purple]',
-							m('span', {onclick: () => flagAdvanced(!flagAdvanced())},
-								'検索オプション'),
-							flagAdvanced() ? [
+						// 検索オプション
+						m('div',
+							m('input[type=checkbox]',
+								m_on('click', 'checked', flagAdvanced)),
+							m('font[color=purple]',
+								m('span', { onclick: () => flagAdvanced(!flagAdvanced()) },
+									'検索オプション'),
+								flagAdvanced() ? [
 
-								m('div', ' 　 　 検索したくない: ',
-									m('input', m_on('change', 'value', textSearchSkip,
-										{autofocus: true, size: 100,
-										 placeholder: '検索したくないファイルやフォルダ'})),
-									' : 検索したくないファイルやフォルダ'),
+									m('div', ' 　 　 検索したくない: ',
+										m('input', m_on('change', 'value', textSearchSkip,
+											{
+												autofocus: true, size: 100,
+												placeholder: '検索したくないファイルやフォルダ'
+											})),
+										' : 検索したくないファイルやフォルダ'),
 
-								m('div',
-									' 　 　 検索時に閉じる: ',
+									m('div',
+										' 　 　 検索時に閉じる: ',
 										m('input', m_on('change', 'value', textClose,
-										{placeholder: '検索時に閉じておくフォルダ', size: 100})),
-									' : 検索時に閉じておくフォルダ'),
-								m('div', ' 　 　 検索最大ファイル数を制限: 　 ',
-									m('span',
-										' 　 最大ファイル数/フォルダ: ',
+											{ placeholder: '検索時に閉じておくフォルダ', size: 100 })),
+										' : 検索時に閉じておくフォルダ'),
+									m('div', ' 　 　 検索最大ファイル数を制限: 　 ',
+										m('span',
+											' 　 最大ファイル数/フォルダ: ',
 											m('input', m_on('change', 'value', textMaxFiles,
-											{placeholder: '最大ファイル数/フォルダ', size: 10}))),
-									m('span',
-										' 　 最大ファイル数/トータル: ',
+												{ placeholder: '最大ファイル数/フォルダ', size: 10 }))),
+										m('span',
+											' 　 最大ファイル数/トータル: ',
 											m('input', m_on('change', 'value', textMaxTotalFiles,
-											{placeholder: '最大ファイル数/トータル', size: 10})))),
-							] : '')),
-					//m('hr'),
+												{ placeholder: '最大ファイル数/トータル', size: 10 })))),
+								] : '')),
+						//m('hr'),
 
-					// 検索結果フィルタ
-					m('div',
-						m('input[type=checkbox]',
-							m_on('click', 'checked', flagFilter)),
-						m('font[color=darkblue]',
-							m('span', {onclick: () => flagFilter(!flagFilter())},
-								'検索結果フィルタ'),
-							flagFilter() ? [
-								m('div', ' 　 　 結果を絞り込み: ',
-									m('input', m_on('change', 'value', textIncludes,
-										{placeholder: '含む', size: 100})),
-									' を含む'),
-								m('div', ' 　 　 結果から非表示: ',
-									m('input', m_on('change', 'value', textExcludes,
-										{placeholder: '含まない', size: 100})),
-									' を含まない'),
-							] : '')),
+						// 検索結果フィルタ
+						m('div',
+							m('input[type=checkbox]',
+								m_on('click', 'checked', flagFilter)),
+							m('font[color=darkblue]',
+								m('span', { onclick: () => flagFilter(!flagFilter()) },
+									'検索結果フィルタ'),
+								flagFilter() ? [
+									m('div', ' 　 　 結果を絞り込み: ',
+										m('input', m_on('change', 'value', textIncludes,
+											{ placeholder: '含む', size: 100 })),
+										' を含む'),
+									m('div', ' 　 　 結果から非表示: ',
+										m('input', m_on('change', 'value', textExcludes,
+											{ placeholder: '含まない', size: 100 })),
+										' を含まない'),
+								] : '')),
 
-				]
+					]
 			),
-			m('hr', {key: 'hr-search'}),
+			m('hr', { key: 'hr-search' }),
 
 			// メッセージ
-			m('div', {key: 'message'}, m('b', textMessage())),
-			m('hr', {key: 'hr-message'}),
+			m('div', { key: 'message' }, m('b', textMessage())),
+			m('hr', { key: 'hr-message' }),
 
 			// 検索結果
 			viewSearchResult(),
-			m('hr', {key: 'hr-result'}),
+			m('hr', { key: 'hr-result' }),
 
 			// 使い方表示
-			m('div', {key: 'usage'},
+			m('div', { key: 'usage' },
 				m('input[type=checkbox]',
 					m_on('click', 'checked', flagUsage)),
 				m('font[color=gray]',
-					{onclick: () => flagUsage(!flagUsage())},
+					{ onclick: () => flagUsage(!flagUsage()) },
 					'使い方',
 					flagUsage() ? viewUsage() : '')),
 
 			// リリース・ノート表示
-			m('div', {key: 'release-notes'},
+			m('div', { key: 'release-notes' },
 				m('input[type=checkbox]',
 					m_on('click', 'checked', flagReleaseNotes)),
 				m('font[color=darkblue]',
-					{onclick: () => flagReleaseNotes(!flagReleaseNotes())},
+					{ onclick: () => flagReleaseNotes(!flagReleaseNotes()) },
 					'リリース・ノート: ' + version,
 					flagReleaseNotes() ? viewReleaseNotes() : '')),
 
 			// やりたいことリスト表示
-			m('div', {key: 'wish-list'},
+			m('div', { key: 'wish-list' },
 				m('input[type=checkbox]',
 					m_on('click', 'checked', flagWishList)),
 				m('font[color=purple]',
-					{onclick: () => flagWishList(!flagWishList())},
+					{ onclick: () => flagWishList(!flagWishList()) },
 					'やりたいことリスト',
 					flagWishList() ? viewWishList() : '')),
 
 			// デバッグ表示
-			m('div', {key: 'debug'},
+			m('div', { key: 'debug' },
 				m('input[type=checkbox]',
 					m_on('click', 'checked', flagDebug)),
 				m('font[color=brown]',
-					{onclick: () => flagDebug(!flagDebug())},
+					{ onclick: () => flagDebug(!flagDebug()) },
 					'デバッグ表示 (環境変数など)',
-				(flagDebug() ? viewDebug() : '')))
+					(flagDebug() ? viewDebug() : '')))
 		];
 	}
 
@@ -273,35 +278,37 @@ void function () {
 			return [
 				keys.filter(prop => !prop.startsWith(INVISIBLE_PROP)).map(prop => {
 					const child = node[prop];
-					const fullPath = (nodePath.join('\\') + '\\' + prop).substr(i === 0);
+					const fullPath = (nodePath.join(path.sep) + path.sep + prop).substr(i === 0);
 					if (prop === ERROR_PROP)
 						return child === undefined ? [] :
-							m('tr', {key: fullPath},
+							m('tr', { key: fullPath },
 								range(i + 1).map(x => m('td.indent', indentSpace)),
-								m('td.error', {colspan: n - i}, child + ''));
+								m('td.error', { colspan: n - i }, child + ''));
 					let vdom = [];
 					if (child ||
-							(!textSearch.rex || textSearch.rex.test(prop)) &&
-							(!textIncludes.rex || textIncludes.rex.test(prop)) &&
-							(!textExcludes.rex || !textExcludes.rex.test(prop))) {
+						(!textSearch.rex || textSearch.rex.test(prop)) &&
+						(!textIncludes.rex || textIncludes.rex.test(prop)) &&
+						(!textExcludes.rex || !textExcludes.rex.test(prop))) {
 
-							vdom.push(m('tr', {key: fullPath}, [
-								range(i).map(x => m('td.indent', indentSpace)),
-								child ?
+						vdom.push(m('tr', { key: fullPath }, [
+							range(i).map(x => m('td.indent', indentSpace)),
+							child ?
 								m('td.indent', {}, m('input[type=checkbox]',
 									m_on('click', 'checked', v =>
 										v === undefined ? !child[HIDE_PROP] :
-										(child[HIDE_PROP] = !v))
+											(child[HIDE_PROP] = !v))
 								)) :
 								m('td.indent.folder',
-									{onclick: () => showItemInFolder(fullPath)},
+									{ onclick: () => showItemInFolder(fullPath) },
 									'■'),
-								m(child ? 'td.folder' : 'td.file', {
-									colspan: n - i,
-									title: fullPath.substring(targetDir.length + 1),
-									onclick: () => openItem(fullPath)
-								}, prop)
-							]));
+							m(child ? 'td.folder' : 'td.file', {
+								colspan: n - i,
+								title: fullPath.substring(targetDir.length + 1),
+								onclick: child && process.platform !== 'win32' ?
+									() => showItemInFolder(fullPath) :
+									() => openItem(fullPath)
+							}, prop)
+						]));
 					}
 					if (child && !child[HIDE_PROP])
 						vdom.push(viewNode(nodePath.concat(prop), child, i + 1, n));
@@ -316,8 +323,8 @@ void function () {
 	// 結果表示 ツリー形式
 	function viewSearchResult() {
 		return m('table',
-			{width: '100%', cellPadding: 0, border: 0, borderColor: 'lightgray', cellSpacing: 0},
-			viewNode([], {[targetDir]: wholeObject}, 0, MAX_INDENT)
+			{ width: '100%', cellPadding: 0, border: 0, borderColor: 'lightgray', cellSpacing: 0 },
+			viewNode([], { [targetDir]: wholeObject }, 0, MAX_INDENT)
 		);
 	}
 
@@ -330,7 +337,7 @@ void function () {
 	function search() {
 		textMessage('検索中...');
 		if (timer) return;
-		aa(function *() {
+		aa(function* () {
 			m.redraw(true);
 			wholeObject = {};
 			timer = setInterval(() => m.redraw(true), 500);
@@ -368,15 +375,16 @@ void function () {
 	function finish() {
 		findController.isCancel = true;
 		if (timer) clearInterval(timer);
-		timer = null; 
+		timer = null;
 		m.redraw(true);
 		setTimeout(() => m.redraw(true), 500);
 	}
 
 	// フォルダやファイルを開く
 	function openItem(file) {
-		spawn('explorer', [file]);
-		//electron.shell.openItem(file);
+		process.platform === 'win32' ?
+			spawn('explorer', [file]) :
+			electron.shell.openItem(file);
 		return;
 	}
 
