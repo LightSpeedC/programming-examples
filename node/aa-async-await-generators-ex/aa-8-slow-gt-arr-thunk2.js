67a5cbcdb715d89f959fce278ea0b8d3b8e79614
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-07-24 07:26:31 +0900

    aa async await with generators

---
+++ node/aa-async-await-generators-ex/aa-8-slow-gt-arr-thunk2.js
@@ -0,0 +1,57 @@
+// Array
+// generator
+// function : generator function or thunk
+// promise or other
+
+const F = 'function', aa = g =>
+	g.constructor === Array ? all(g) :
+	cb => (
+		//typeof g === F || typeof g.next === F ?
+
+			((g = typeof g === F ? g(cb) : g), g && typeof g.next === F && (
+			g && typeof g.next === F ?
+				(new Promise((Y, N) => (
+					(function z(e, v) {
+						try {
+							(v = e ? g.throw(e) : g.next(v)).done ?
+								Y(v.value) :
+								aa(v.value)(z);
+						} catch (e) { N(e); }
+					})()
+				))) :
+				Promise.resolve(g)
+			).then(v => cb(null, v), cb)
+		)
+	);
+const all = arr => (cb, n, r) => ((n = arr.length) ? (r = new Array(n), arr.forEach((v, i, z) => (
+	z = (e, v) => e ? (cb(e), n = 0) : (r[i] = v, --n || cb(null, r)),
+	typeof v === F ? (v = v(z), v && v.next && aa(v)(z)) : aa(v)(z)))) : cb(null, []));
+// const t2promise = t => new Promise((res, rej) => t((e, v) => e ? rej(e) : res(v)));
+// const p2thunk = p => cb => p.then(v => cb(null, v), cb);
+
+aa(function* () {
+	console.log('aaa');
+	yield cb => setTimeout(cb, 1000);
+	console.log('bbb');
+	console.log('ccc', yield [
+		cb => setTimeout(cb, 1000, null, 'A'),
+		function (cb) { setTimeout(cb, 100, null, 'B'); },
+		function* () { return 'C'; },
+		function* () { return 'D'; }(),
+	]);
+	/*
+	yield cb => setTimeout(cb, 500);
+	console.log('ddd', yield {
+		a: cb => setTimeout(cb, 1000, null, 'A'),
+		b: function (cb) { setTimeout(cb, 100, null, 'B'); },
+		c: function *() { return 'C'; },
+		d: function *() { return 'D'; } (),
+	});
+	*/
+	yield cb => setTimeout(cb, 1000);
+	console.log('eee', yield []);
+	yield cb => setTimeout(cb, 1000);
+	console.log('fff', yield {});
+	yield cb => setTimeout(cb, 1000);
+	console.log('ggg', yield 'ggg');
+})((e, v) => e ? console.log('fin err:', e) : console.log('fin val:', v));
