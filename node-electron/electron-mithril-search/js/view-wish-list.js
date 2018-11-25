654519d5f477df60943085a936ededea63e129b8
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2018-11-26 00:45:41 +0900

    [update] add into wish list

--- node-electron/electron-mithril-search/js/view-wish-list.js
+++ node-electron/electron-mithril-search/js/view-wish-list.js
@@ -10,6 +10,11 @@ module.exports = function (m) {
 			'更新日付で範囲検索したい',
 			'更新日付を表示したい',
 			'検索ワードのハイライト表示',
+			'ファイルの内容で検索したい',
+			'Mithril v1系にバージョンアップ',
+			'React v15版を作る',
+			'JSX対応 TypeScript v2.4, Babel v6, Webpack v3対応',
+			'更新速度をUP',
 			'いろいろと実現した。後、やることは?',
 		];
 		return m('ul', list.map(x => m('li', x)));
