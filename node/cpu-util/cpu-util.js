2b5e1140cd24605aeae7a4987c51185a3256c2cc
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2020-05-23 08:31:40 +0900

    [update] revise

--- node/cpu-util/cpu-util.js
+++ node/cpu-util/cpu-util.js
@@ -3,27 +3,48 @@
 'use strict';
 
 const os = require('os');
+const DateTime = require('./date-time-string');
+const toDateTimeString = DateTime.toDateTimeString;
 
 const prev = os.cpus();
 const TYPES = Object.keys(prev[0].times);
 
-function getInfo() {
-	return {
-		cpus: os.cpus().map((cpu, i) => {
-			const delta = TYPES.reduce((delta, type) => {
-				delta[type] = cpu.times[type] - prev[i].times[type];
-				prev[i].times[type] = cpu.times[type];
-				return delta;
-			}, { });
-		const total = TYPES.reduce((total, type) => total + delta[type], 0);
-		return TYPES.reduce((load, type) => (load[type] = Math.round(delta[type] / total * 1000) / 10, load), {});
+const round = x => Math.round(x * 10) / 10;
+const fmtPct = x => ('  ' + round(x).toFixed(1) + '%').substr(-6);
+
+const getInfo = () => ({
+	cpus: os.cpus().map((cpu, i) => {
+		const delta = TYPES.reduce((delta, type) => {
+			delta[type] = cpu.times[type] - prev[i].times[type];
+			prev[i].times[type] = cpu.times[type];
+			return delta;
+		}, { });
+		const sum = TYPES.reduce((sum, k) => sum + delta[k], 0);
+		return TYPES.reduce((load, k) => (load[k] = delta[k] / sum * 100, load),
+			{util: (sum - delta.idle) / sum * 100});
 	}),
-	// totalmem: os.totalmem(),
-	// freemem: os.freemem(),
+	memory: {util: (os.totalmem() - os.freemem()) / os.totalmem() * 100,
+		totalmem: os.totalmem(), freemem: os.freemem()},
 	// uptime: os.uptime(),
 	// loadavg: os.loadavg(),		// 1, 5, and 15 min's load avg
-	timestamp: new Date().toLocaleString(),
-	};
+	timestamp: toDateTimeString(),
+});
+
+function showInfo() {
+	const info = getInfo();
+	const cpuUtil = info.cpus.reduce((sum, cpu) => sum + cpu.util, 0) / info.cpus.length;
+
+	console.log(process.env.HOSTNAME || process.env.COMPUTERNAME,
+		'■'.repeat(cpuUtil / 5) +
+		'□'.repeat((100 - cpuUtil) / 5),
+		'cpu:' + fmtPct(cpuUtil),
+		'mem:' + fmtPct(info.memory.util),
+		info.timestamp);
 }
 
-setInterval(() => console.log(getInfo()), 5000);
+setTimeout(showInfo, 100);
+setTimeout(showInfo, 400);
+setTimeout(showInfo, 1000);
+setTimeout(showInfo, 4000);
+setTimeout(showInfo, 10 * 1000);
+setInterval(showInfo, 30 * 1000);
