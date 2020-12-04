9fec2670167d3bd597aebcf87e4197ce87004ac9
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2020-12-05 08:03:38 +0900

    [add] cookie session, history push/replace example

---
+++ html/cookie-ex/cookie-session-ex/cookie-session-by-window/http-server-by-python.bat
@@ -0,0 +1,8 @@
+rem “ú–{Œê
+start "web" http://localhost:3000/index.html
+python -m SimpleHTTPServer 3000
+rem python3 -m http.server 3000
+rem https://qiita.com/sudahiroshi/items/e74d61d939f18779970d
+rem (/cgi-bin/index.py) python -m CGIHTTPServer 9000
+rem (public/index.html) npm install -g http-server & http-server -p 8000
+pause
