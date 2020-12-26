e680316de969b08b77029b7656c467e1cb481099
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2020-12-26 11:32:27 +0900

    [update] code-conv.js --- var -> const | let

--- node/code-conv/code-conv.js
+++ node/code-conv/code-conv.js
@@ -3,8 +3,8 @@
 
 'use strict';
 
-var mapSj2Uni = createMap(require('./code-conv-sjis-uni'));
-var mapUni2Sj = createMap(require('./code-conv-uni-sjisnaiji-nkf'));
+const mapSj2Uni = createMap(require('./code-conv-sjis-uni'));
+const mapUni2Sj = createMap(require('./code-conv-uni-sjisnaiji-nkf'));
 
 //for (var i in mapSj2Uni)
 //  console.log(Number(i).toString(16) + ':' + String.fromCharCode(mapSj2Uni[i]));
@@ -22,7 +22,7 @@ function createMap(codePairList) {
   if (codePairList[codePairList.length - 1] !== (codePairList.length - 1) / 2)
     throw new Error('end of code pair list must be length');
 
-  var map = {};
+  const map = {};
 
   for (var i = 0, n = codePairList.length; i < n; i += 2)
     map[codePairList[i]] = codePairList[i + 1];
@@ -48,24 +48,24 @@ function convSj2UniSync(buffSj) {
   if (typeof buffSj !== 'object' || !(buffSj instanceof Buffer))
     throw new Error('buffSj must be a buffer');
 
-  var strUni = '';
+  let strUni = '';
 
-  for (var i = 0, n = buffSj.length; i < n; ) {
-    var c1 = buffSj[i++];
+  for (let i = 0, n = buffSj.length; i < n; ) {
+    const c1 = buffSj[i++];
     if (c1 <= 0x80 || c1 >= 0xFD) {
       strUni += String.fromCharCode(c1);
       continue;
     }
     if (c1 >= 0xA0 && c1 <= 0xDF) {
-      var c10 = mapSj2Uni[c1];
+      const c10 = mapSj2Uni[c1];
       if (c10) strUni += String.fromCharCode(c10);
       else strUni += '?';
       continue;
     }
     if (i >= n) break;
-    var c2 = buffSj[i++];
-    var c = (c1 << 8) | c2;
-    var c0 = mapSj2Uni[c];
+    const c2 = buffSj[i++];
+    const c = (c1 << 8) | c2;
+    const c0 = mapSj2Uni[c];
     if (c0) strUni += String.fromCharCode(c0);
     else strUni += '■';
   }
@@ -83,10 +83,10 @@ function convUni2SjSync(strUni) {
   if (typeof strUni !== 'string')
     throw new Error('strUni must be a buffer or string');
 
-  var listSj = [];
+  const listSj = [];
 
-  for (var i = 0, n = strUni.length; i < n; ++i) {
-    var c1 = strUni.charCodeAt(i);
+  for (let i = 0, n = strUni.length; i < n; ++i) {
+    const c1 = strUni.charCodeAt(i);
 
     // ASCII JIS Roman 半角ASCII/JIS Roman
     if (c1 <= 0x7F) {
@@ -96,21 +96,21 @@ function convUni2SjSync(strUni) {
 
     // JIS Hankaku Katakana 半角ｶﾀｶﾅ
     if (c1 >= 0xFF61 && c1 <= 0xFF9F) {
-      var c10 = mapUni2Sj[c1];
+      const c10 = mapUni2Sj[c1];
       if (c10) listSj.push(c10);
       else listSj.push(0x3F); // ?
       continue;
     }
 
     // JIS Zenkaku Kanji 全角漢字
-    var c10 = mapUni2Sj[c1];
+    const c10 = mapUni2Sj[c1];
     if (!c10) {
       listSj.push(0x81); // ■
       listSj.push(0xA1); // ■
       continue;
     }
 
-    var c10h = c10 >>> 8;
+    const c10h = c10 >>> 8;
     if (c10h > 0)
       listSj.push(c10h);
     listSj.push(c10 & 0xFF);
@@ -123,8 +123,8 @@ CodeConv.convUni2SjSync = convUni2SjSync;
 //######################################################################
 // method strUni = convAny2UniSync(buffAny)
 function convAny2UniSync(buffAny) {
-  var strUniFromSj = convSj2UniSync(buffAny);
-  var strUniFromUni = buffAny.toString();
+  const strUniFromSj = convSj2UniSync(buffAny);
+  const strUniFromUni = buffAny.toString();
 
   if (strUniFromSj.length < strUniFromUni.length)
     return strUniFromSj;
