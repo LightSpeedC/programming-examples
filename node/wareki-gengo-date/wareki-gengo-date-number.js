32b5bd791736eb543c6d31d8cfc341db5ef53091
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2022-11-13 15:26:50 +0900

    [update] wareki.

--- node/wareki-gengo-date/wareki-gengo-date-number.js
+++ node/wareki-gengo-date/wareki-gengo-date-number.js
@@ -1,33 +1,39 @@
+// @ts-check
+
 // SEE ALSO:
 // https://www.npmjs.com/package/wareki
 // https://github.com/tohashi/wareki
 // https://github.com/tohashi/wareki/blob/master/src/wareki.js
 
 const gengos = [
-	{from:18680125, to:19120729, name:'明治', ryaku:'㍾', alpha:'M'},
-	{from:19120730, to:19261224, name:'大正', ryaku:'㍽', alpha:'T'},
-	{from:19261225, to:19890107, name:'昭和', ryaku:'㍼', alpha:'S'},
-	{from:19890108, to:20190430, name:'平成', ryaku:'㍻', alpha:'H'},
-	{from:20190501, to:99999999, name:'令和', ryaku:'㋿', alpha:'R'},
+	{from:18680125, to:19120729, name:'明治', ryaku:'㍾', alpha:'M', year:0},
+	{from:19120730, to:19261224, name:'大正', ryaku:'㍽', alpha:'T', year:0},
+	{from:19261225, to:19890107, name:'昭和', ryaku:'㍼', alpha:'S', year:0},
+	{from:19890108, to:20190430, name:'平成', ryaku:'㍻', alpha:'H', year:0},
+	{from:20190501, to:99999999, name:'令和', ryaku:'㋿', alpha:'R', year:0},
 ];
 gengos.forEach(x => x.year = Math.floor(x.from / 10000) - 1);
-//console.log(gengos);
+gengos.forEach(x => console.log(JSON.stringify(x)));
 
 function date2ymd(dt) {
 	return dt.getFullYear() * 10000 + dt.getMonth() * 100 + 100 + dt.getDate();
 }
 
 function gan(y) {
-	return y === 1 ? '元' : y;
+	return y === 1 ? '元' : zpad(y, 2);
 }
 function ymd2wareki(ymd) {
 	const gengo = gengos.filter(x => x.from <= ymd && ymd <= x.to)[0];
-	if (!gengo) return ymd;
+	if (!gengo) return ymd + '\t西暦' + Math.floor(ymd / 10000) +
+	'年' + zpad(Math.floor(ymd / 100) % 100, 2) + '月' + zpad(ymd % 100, 2) + '日\t変換できない';
 	return ymd + '\t' + gengo.name + gan(Math.floor(ymd / 10000) - gengo.year) +
-		'年' + Math.floor(ymd / 100) % 100 + '月' + ymd % 100 + '日';
+		'年' + zpad(Math.floor(ymd / 100) % 100, 2) + '月' + zpad(ymd % 100, 2) + '日';
 }
 
-console.log(date2ymd(new Date));
+function zpad(n, m) {
+	return ('0000' + n).substr(-m);
+}
+
 console.log(ymd2wareki(date2ymd(new Date)));
 console.log(ymd2wareki(18680124));
 console.log(ymd2wareki(18680125));
@@ -41,3 +47,6 @@ console.log(ymd2wareki(20181231));
 console.log(ymd2wareki(20190101));
 console.log(ymd2wareki(20190430));
 console.log(ymd2wareki(20190501));
+console.log(ymd2wareki(20200501));
+console.log(ymd2wareki(20210501));
+console.log(ymd2wareki(20220501));
--- node/wareki-gengo-date/wareki-gengo-date-string.js
+++ node/wareki-gengo-date/wareki-gengo-date-string.js
@@ -1,43 +1,52 @@
+// @ts-check
+
 // SEE ALSO:
 // https://www.npmjs.com/package/wareki
 // https://github.com/tohashi/wareki
 // https://github.com/tohashi/wareki/blob/master/src/wareki.js
 
 const gengos = [
-	{from:18680125, to:19120729, name:'明治', ryaku:'㍾', alpha:'M'},
-	{from:19120730, to:19261224, name:'大正', ryaku:'㍽', alpha:'T'},
-	{from:19261225, to:19890107, name:'昭和', ryaku:'㍼', alpha:'S'},
-	{from:19890108, to:20190430, name:'平成', ryaku:'㍻', alpha:'H'},
-	{from:20190501, to:99999999, name:'令和', ryaku:'㋿', alpha:'R'},
+	{from:'18680125', to:'19120729', name:'明治', ryaku:'㍾', alpha:'M', year:0},
+	{from:'19120730', to:'19261224', name:'大正', ryaku:'㍽', alpha:'T', year:0},
+	{from:'19261225', to:'19890107', name:'昭和', ryaku:'㍼', alpha:'S', year:0},
+	{from:'19890108', to:'20190430', name:'平成', ryaku:'㍻', alpha:'H', year:0},
+	{from:'20190501', to:'99999999', name:'令和', ryaku:'㋿', alpha:'R', year:0},
 ];
-gengos.forEach(x => x.year = Math.floor(x.from / 10000) - 1);
-//console.log(gengos);
+gengos.forEach(x => x.year = Number(x.from.substring(0, 4)) - 1);
+gengos.forEach(x => console.log(JSON.stringify(x)));
 
 function date2ymd(dt) {
-	return dt.getFullYear() * 10000 + dt.getMonth() * 100 + 100 + dt.getDate();
+	return dt.getFullYear() + zpad(dt.getMonth() + 1, 2) + zpad(dt.getDate(), 2);
 }
 
 function gan(y) {
-	return y === 1 ? '元' : y;
+	return Number(y) === 1 ? '元' : y;
 }
 function ymd2wareki(ymd) {
 	const gengo = gengos.filter(x => x.from <= ymd && ymd <= x.to)[0];
-	if (!gengo) return ymd;
-	return ymd + '\t' + gengo.name + gan(Math.floor(ymd / 10000) - gengo.year) +
-		'年' + Math.floor(ymd / 100) % 100 + '月' + ymd % 100 + '日';
+	if (!gengo) return ymd + '\t西暦' + ymd.substring(0, 4) +
+		'年' + ymd.substring(4, 6) + '月' + ymd.substring(6, 8) + '日\t変換できない';
+	return ymd + '\t' + gengo.name + gan(zpad(ymd.substring(0, 4) - gengo.year, 2)) +
+		'年' + ymd.substring(4, 6) + '月' + ymd.substring(6, 8) + '日';
 }
 
-console.log(date2ymd(new Date));
+function zpad(n, m) {
+	return ('0000' + n).substr(-m);
+}
+
 console.log(ymd2wareki(date2ymd(new Date)));
-console.log(ymd2wareki(18680124));
-console.log(ymd2wareki(18680125));
-console.log(ymd2wareki(19120729));
-console.log(ymd2wareki(19120730));
-console.log(ymd2wareki(19261224));
-console.log(ymd2wareki(19261225));
-console.log(ymd2wareki(19890107));
-console.log(ymd2wareki(19890108));
-console.log(ymd2wareki(20181231));
-console.log(ymd2wareki(20190101));
-console.log(ymd2wareki(20190430));
-console.log(ymd2wareki(20190501));
+console.log(ymd2wareki('18680124'));
+console.log(ymd2wareki('18680125'));
+console.log(ymd2wareki('19120729'));
+console.log(ymd2wareki('19120730'));
+console.log(ymd2wareki('19261224'));
+console.log(ymd2wareki('19261225'));
+console.log(ymd2wareki('19890107'));
+console.log(ymd2wareki('19890108'));
+console.log(ymd2wareki('20181231'));
+console.log(ymd2wareki('20190101'));
+console.log(ymd2wareki('20190430'));
+console.log(ymd2wareki('20190501'));
+console.log(ymd2wareki('20200501'));
+console.log(ymd2wareki('20210501'));
+console.log(ymd2wareki('20220501'));
